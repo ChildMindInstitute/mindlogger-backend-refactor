711188f85e6f3845fbb64f2c97f7015a7e1bade4
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2022-11-22 10:27:00 +0200

    Base model update

--- src/infrastructure/database/base.py
+++ src/infrastructure/database/base.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Integer
 from sqlalchemy.orm import declarative_base
 
 from infrastructure.database.core import engine
@@ -10,6 +12,9 @@ class _Base:
     """Base class for all database models."""
 
     id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    is_deleted = Column(Boolean, default=False)
 
 
 Base = declarative_base(cls=_Base, bind=engine.sync_engine)
