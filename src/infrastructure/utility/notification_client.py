eb5e3f43c87fa93341d8d6631395f339f30df2d5
Damirkhon Aloev <aloevdamirkhon@gmail.com>
2022-11-30 17:03:04 +0500

    fix:changed structure of Notification according to  suggestions

--- src/infrastructure/utility/notification_client.py
+++ src/infrastructure/utility/notification_client.py
@@ -5,11 +5,37 @@ from pyfcm import FCMNotification
 from concurrent.futures.thread import ThreadPoolExecutor
 
 
-class FirebaseNotification(FCMNotification):
+class RetryException(Exception):
+    def __init__(self, timeout):
+        self.timeout = timeout
+
+
+class _FirebaseNotification(FCMNotification):
+    def do_request(self, payload, timeout=5):
+        response = self.requests_session.post(
+            self.FCM_END_POINT, data=payload, timeout=timeout
+        )
+        if (
+            "Retry-After" in response.headers
+            and int(response.headers["Retry-After"]) > 0
+        ):
+            raise RetryException(timeout=timeout)
+        return response
+
+    def send_request(self, payloads=None, timeout=None):
+
+        self.send_request_responses = []
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            response = executor.map(self.do_request, payloads)
+            executor.map(self.send_request_responses.append, response)
+
+
+class Notification:
     """SIngleton FCM Notification client"""
 
     _initialized: bool = False
     _instance: None = None
+    client: _FirebaseNotification = None
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
@@ -21,27 +47,20 @@ class FirebaseNotification(FCMNotification):
         if self._initialized:
             return
 
-        super(FCMNotification, self).__init__(
-            api_key=settings.notification.api_key
-        )
+        self.client = _FirebaseNotification(api_key=config.api_key)
 
         self._initialized = True
 
-    def do_request(self, payload, timeout=5):
-        response = self.requests_session.post(
-            self.FCM_END_POINT, data=payload, timeout=timeout
-        )
-        if (
-            "Retry-After" in response.headers
-            and int(response.headers["Retry-After"]) > 0
-        ):
-            sleep_time = int(response.headers["Retry-After"])
-            asyncio.sleep(sleep_time)
-            return self.do_request(payload, timeout)
-        return response
-
-    def send_request(self, payloads=None, timeout=None):
-        self.send_request_responses = []
-        with ThreadPoolExecutor(max_workers=2) as executor:
-            response = executor.map(self.do_request, payloads)
-            executor.map(self.send_request_responses.append, response)
+    async def notify(self, devices: list = None, *args, **kwargs):
+        try:
+            if len(devices) > 1:
+                self.client.notify_multiple_devices(
+                    registration_ids=devices, *args, **kwargs
+                )
+            else:
+                self.client.notify_single_device(
+                    registration_id=devices[0], *args, **kwargs
+                )
+        except RetryException as ex:
+            await asyncio.sleep(ex.timeout)
+            self.notify(devices, *args, **kwargs)
