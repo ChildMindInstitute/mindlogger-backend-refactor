210314340c4d83f4835acb1fe99915a6ec482084
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-01-12 05:17:50 +0200

    WIP

--- src/apps/users/tests/test_user.py
+++ src/apps/users/tests/test_user.py
@@ -1,41 +1,35 @@
 from starlette import status
 
+from apps.authentication.router import router as auth_router
 from apps.shared.domain import Response
 from apps.shared.test import BaseTest
 from apps.users import UserSchema, UsersCRUD
 from apps.users.domain import (
     PublicUser,
     User,
-    UserCreateRequest,
     UserLoginRequest,
     UserUpdateRequest,
 )
+from apps.users.router import router as user_router
+from apps.users.tests.factories import UserCreateRequestFactory
 from infrastructure.database import transaction
 
 
 class TestUser(BaseTest):
     fixtures = ["users/fixtures/users.json"]
 
-    login_url = "/auth/token"
-    user_create_url = "/users"
-    user_retrieve_url = "/users/me"
-    user_update_url = "/users/me"
-    user_delete_url = "/users/me"
-    password_update_url = "/users/me/password"
-    password_recovery_url = "/users/me/password/recover"
-    password_recovery_approve_url = "/users/me/password/recover/approve"
-
-    create_request_user_new: UserCreateRequest = UserCreateRequest(
-        email="tom_new@mindlogger.com",
-        full_name="Tom Isaak",
-        password="Test1234!",
+    get_token_url = auth_router.url_path_for("get_token")
+    user_create_url = user_router.url_path_for("user_create")
+    user_retrieve_url = user_router.url_path_for("user_retrieve")
+    user_update_url = user_router.url_path_for("user_update")
+    user_delete_url = user_router.url_path_for("user_delete")
+    password_update_url = user_router.url_path_for("password_update")
+    password_recovery_url = user_router.url_path_for("password_recovery")
+    password_recovery_approve_url = user_router.url_path_for(
+        "password_recovery_approve"
     )
 
-    create_request_user_exist: UserCreateRequest = UserCreateRequest(
-        email="tom@mindlogger.com",
-        full_name="Tom Isaak",
-        password="Test1234!",
-    )
+    create_request_user = UserCreateRequestFactory.build()
 
     login_request_user: UserLoginRequest = UserLoginRequest(
         email="tom@mindlogger.com",
@@ -50,10 +44,10 @@ class TestUser(BaseTest):
     async def test_creating_user(self):
         # Creating new user
         response = await self.client.post(
-            self.user_create_url, data=self.create_request_user_new.dict()
+            self.user_create_url, data=self.create_request_user.dict()
         )
         created_user: User = await UsersCRUD().get_by_email(
-            self.create_request_user_new.email
+            self.create_request_user.email
         )
 
         public_user = PublicUser(**created_user.dict())
@@ -62,14 +56,19 @@ class TestUser(BaseTest):
 
         count = await UsersCRUD().count()
 
-        assert response.status_code == status.HTTP_201_CREATED, expected_result
+        assert response.status_code == status.HTTP_201_CREATED
+        # assert response.json() == expected_result.dict()
         assert count == expected_result.result.id
 
     @transaction.rollback
     async def test_creating_user_exist(self):
+        # Creating new user
+        await self.client.post(
+            self.user_create_url, data=self.create_request_user.dict()
+        )
         # Creating a user that already exists
         response = await self.client.post(
-            self.user_create_url, data=self.create_request_user_exist.dict()
+            self.user_create_url, data=self.create_request_user.dict()
         )
 
         expected_result = {"messages": ["User already exists"]}
@@ -79,29 +78,42 @@ class TestUser(BaseTest):
 
     @transaction.rollback
     async def test_retrieving_user(self):
-        await self.client.login(
-            self.login_url,
-            self.login_request_user.email,
-            self.login_request_user.password,
+        # Creating new user
+        await self.client.post(
+            self.user_create_url, data=self.create_request_user.dict()
+        )
+        # User get token
+        response = await self.client.post(
+            url=self.get_token_url,
+            data=dict(
+                email=self.create_request_user.email,
+                password=self.create_request_user.password,
+            ),
         )
 
+        access_token = response.json()["Result"]["AccessToken"]
+        token_type = response.json()["Result"]["TokenType"]
+        self.client.headers["Authorization"] = f"{token_type} {access_token}"
+
+        # User retrieve
         response = await self.client.get(self.user_retrieve_url)
 
         logged_in_user: User = await UsersCRUD().get_by_email(
-            self.login_request_user.email
+            self.create_request_user.email
         )
 
         public_user = PublicUser(**logged_in_user.dict())
 
         expected_result: Response[PublicUser] = Response(result=public_user)
 
-        assert response.status_code == status.HTTP_200_OK, expected_result
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected_result.dict()
         assert response.json()["Result"]["Id"] == expected_result.result.id
 
     @transaction.rollback
     async def test_updating_user(self):
         await self.client.login(
-            self.login_url,
+            self.get_token_url,
             self.login_request_user.email,
             self.login_request_user.password,
         )
@@ -118,13 +130,14 @@ class TestUser(BaseTest):
 
         expected_result: Response[PublicUser] = Response(result=public_user)
 
-        assert response.status_code == status.HTTP_200_OK, expected_result
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == expected_result.dict()
         assert response.json()["Result"]["Id"] == expected_result.result.id
 
     @transaction.rollback
     async def test_deleting_user(self):
         await self.client.login(
-            self.login_url,
+            self.get_token_url,
             self.login_request_user.email,
             self.login_request_user.password,
         )
@@ -137,6 +150,6 @@ class TestUser(BaseTest):
             key="email", value=self.login_request_user.email
         )
 
-        assert instance.is_deleted is True
+        assert instance.is_deleted
         assert response.status_code == status.HTTP_204_NO_CONTENT
-        assert response.content == b""
+        assert not response.content
