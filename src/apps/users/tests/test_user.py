3152f7626a18ea477769dae450f5d15fee19e596
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-01-12 23:33:16 +0200

    WIP

--- src/apps/users/tests/test_user.py
+++ src/apps/users/tests/test_user.py
@@ -4,15 +4,9 @@ from apps.authentication.router import router as auth_router
 from apps.shared.domain import Response
 from apps.shared.test import BaseTest
 from apps.users import UserSchema, UsersCRUD
-from apps.users.domain import (
-    PasswordRecoveryRequest,
-    PublicUser,
-    User,
-    UserLoginRequest,
-)
+from apps.users.domain import PublicUser, User, UserLoginRequest
 from apps.users.router import router as user_router
 from apps.users.tests.factories import (
-    PasswordUpdateRequestFactory,
     UserCreateRequestFactory,
     UserUpdateRequestFactory,
 )
@@ -20,26 +14,15 @@ from infrastructure.database import transaction
 
 
 class TestUser(BaseTest):
-    # fixtures = ["users/fixtures/users.json"]
 
     get_token_url = auth_router.url_path_for("get_token")
     user_create_url = user_router.url_path_for("user_create")
     user_retrieve_url = user_router.url_path_for("user_retrieve")
     user_update_url = user_router.url_path_for("user_update")
     user_delete_url = user_router.url_path_for("user_delete")
-    password_update_url = user_router.url_path_for("password_update")
-    password_recovery_url = user_router.url_path_for("password_recovery")
-    password_recovery_approve_url = user_router.url_path_for(
-        "password_recovery_approve"
-    )
 
     create_request_user = UserCreateRequestFactory.build()
     user_update_request = UserUpdateRequestFactory.build()
-    password_update_request = PasswordUpdateRequestFactory.build()
-
-    password_recovery_request: PasswordRecoveryRequest = (
-        PasswordRecoveryRequest(email="tom@mindlogger.com")
-    )
 
     @transaction.rollback
     async def test_creating_user(self):
@@ -169,55 +152,3 @@ class TestUser(BaseTest):
         assert instance.is_deleted
         assert response.status_code == status.HTTP_204_NO_CONTENT
         assert not response.content
-
-    @transaction.rollback
-    async def test_updating_password(self):
-        # Creating new user
-        await self.client.post(
-            self.user_create_url, data=self.create_request_user.dict()
-        )
-
-        login_request_user: UserLoginRequest = UserLoginRequest(
-            **self.create_request_user.dict()
-        )
-
-        # User get token
-        await self.client.get_token(
-            url=self.get_token_url,
-            user_login_request=login_request_user,
-        )
-
-        # Password update
-        response = await self.client.put(
-            self.password_update_url, data=self.password_update_request.dict()
-        )
-
-        updated_user: User = await UsersCRUD().get_by_email(
-            login_request_user.email
-        )
-
-        public_user = PublicUser(**updated_user.dict())
-
-        expected_result: Response[PublicUser] = Response(result=public_user)
-
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == expected_result.dict(by_alias=True)
-
-        # User get token with new password
-        login_request_user: UserLoginRequest = UserLoginRequest(
-            email=self.create_request_user.dict()["email"],
-            password=self.password_update_request.dict()["password"],
-        )
-
-        response = await self.client.get_token(
-            url=self.get_token_url,
-            user_login_request=login_request_user,
-        )
-
-        assert response.status_code == status.HTTP_200_OK
-
-    async def test_recovery_password(self):
-        pass
-
-    async def test_recovery_password_approve(self):
-        pass
