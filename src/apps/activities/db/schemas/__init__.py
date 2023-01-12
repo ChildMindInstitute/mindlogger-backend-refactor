d33c552015fa5ac4d87c3a509c8c0a561674ccc6
tom <isoqov12@gmail.com>
2023-01-12 12:48:37 +0500

    add history structure

--- src/apps/activities/db/schemas/__init__.py
+++ src/apps/activities/db/schemas/__init__.py
@@ -1,10 +1,12 @@
 from apps.activities.db.schemas.activity import (  # noqa: F401, F403
     ActivityHistorySchema,
     ActivitySchema,
+    ActivityHistorySchema
 )
 from apps.activities.db.schemas.activity_item import (  # noqa: F401, F403
     ActivityItemHistorySchema,
     ActivityItemSchema,
+    ActivityItemHistorySchema
 )
 from apps.activities.db.schemas.reusable_item_choices import (  # noqa: F401
     ReusableItemChoiceSchema,
