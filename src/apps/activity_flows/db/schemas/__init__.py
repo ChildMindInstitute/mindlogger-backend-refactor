968e882b304872ecc695008d6260bdd026a8379d
tom <isoqov12@gmail.com>
2023-01-12 12:48:37 +0500

    add history structure

--- src/apps/activity_flows/db/schemas/__init__.py
+++ src/apps/activity_flows/db/schemas/__init__.py
@@ -1,8 +1,10 @@
 from apps.activity_flows.db.schemas.flow import (  # noqa: F401, F403
     ActivityFlowHistoriesSchema,
     ActivityFlowSchema,
+    ActivityFlowHistoriesSchema
 )
 from apps.activity_flows.db.schemas.flow_item import (  # noqa: F401, F403
     ActivityFlowItemHistorySchema,
     ActivityFlowItemSchema,
+    ActivityFlowItemHistorySchema
 )
