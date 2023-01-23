1b5663a2ea1822dc6b958a2f73aa2171303a47bb
tom <isoqov12@gmail.com>
2023-01-23 18:10:27 +0500

    fix code quality

--- src/apps/applets/domain/history.py
+++ src/apps/applets/domain/history.py
@@ -62,6 +62,4 @@ class PublicAppletHistoryChange(PublicModel):
     report_include_user_id: str | None
     report_include_case_id: str | None
     report_email_body: str | None
-    activities: list[PublicActivityHistoryChange] = Field(
-        default_factory=list
-    )
+    activities: list[PublicActivityHistoryChange] = Field(default_factory=list)
