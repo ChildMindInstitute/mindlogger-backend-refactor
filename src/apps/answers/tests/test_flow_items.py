f5e7f8597daaf1d1a18c8874f176fb389373eb19
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-02-03 15:23:56 +0200

    Added test for activity and flow

--- src/apps/answers/tests/test_flow_items.py
+++ src/apps/answers/tests/test_flow_items.py
@@ -1,10 +1,10 @@
-from apps.answers.router import router as answer_activity_router
+from apps.answers.router import router as answer_flow_router
 from apps.authentication.router import router as auth_router
 from apps.shared.test import BaseTest
 from infrastructure.database import transaction
 
 
-class TestAnswerActivityItems(BaseTest):
+class TestFlowActivityItems(BaseTest):
     fixtures = [
         "users/fixtures/users.json",
         "applets/fixtures/applets.json",
@@ -15,15 +15,18 @@ class TestAnswerActivityItems(BaseTest):
         "activities/fixtures/activity_items.json",
         "activities/fixtures/activity_item_histories.json",
         "activity_flows/fixtures/activity_flows.json",
+        "activity_flows/fixtures/activity_flow_histories.json",
+        "activity_flows/fixtures/activity_flow_items.json",
+        "activity_flows/fixtures/activity_flow_item_histories.json",
     ]
 
     login_url = auth_router.url_path_for("get_token")
-    answer_activity_item_create_url = answer_activity_router.url_path_for(
-        "answer_activity_item_create"
+    answer_flow_item_create_url = answer_flow_router.url_path_for(
+        "answer_flow_item_create"
     )
 
     @transaction.rollback
-    async def test_answer_activity_items_create_for_respondent(self):
+    async def test_answer_flow_items_create_for_respondent(self):
         await self.client.login(
             self.login_url, "tom@mindlogger.com", "Test1234!"
         )
@@ -31,7 +34,7 @@ class TestAnswerActivityItems(BaseTest):
         create_data = dict(
             appletId=1,
             appletHistoryVersion="1.0.0",
-            activityId=1,
+            flowItemHistoryId=1,
             answers=[
                 dict(
                     activityItemHistoryId=1,
@@ -45,19 +48,19 @@ class TestAnswerActivityItems(BaseTest):
         )
 
         response = await self.client.post(
-            self.answer_activity_item_create_url, data=create_data
+            self.answer_flow_item_create_url, data=create_data
         )
 
         assert response.status_code == 201, response.json()
 
     @transaction.rollback
-    async def test_answer_activity_items_create_for_not_respondent(self):
+    async def test_answer_flow_items_create_for_not_respondent(self):
         await self.client.login(self.login_url, "bob@gmail.com", "Test1234!")
 
         create_data = dict(
             appletId=1,
             appletHistoryVersion="1.0.0",
-            activityId=1,
+            flowItemHistoryId=1,
             answers=[
                 dict(
                     activityItemHistoryId=1,
@@ -71,7 +74,7 @@ class TestAnswerActivityItems(BaseTest):
         )
 
         response = await self.client.post(
-            self.answer_activity_item_create_url, data=create_data
+            self.answer_flow_item_create_url, data=create_data
         )
 
         assert response.status_code == 400, response.json()
