f72a29c548824dffe24c89b5fc7e150f5a3fc340
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-02-17 14:14:27 +0200

    WIP

--- src/apps/users/db/schemas.py
+++ src/apps/users/db/schemas.py
@@ -8,4 +8,6 @@ class UserSchema(Base):
 
     email = Column(String(length=100), unique=True)
     full_name = Column(String(length=100))
+    first_name = Column(String(length=50))
+    last_name = Column(String(length=50))
     hashed_password = Column(String(length=100))
