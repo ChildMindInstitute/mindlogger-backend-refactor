887119b2f69adc33c6991fd357389e1d1dd2c072
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-02-21 20:32:59 +0200

    Removed nickname from user - Fixed migrations

--- src/apps/users/db/schemas.py
+++ src/apps/users/db/schemas.py
@@ -9,5 +9,4 @@ class UserSchema(Base):
     email = Column(String(length=100), unique=True)
     first_name = Column(String(length=50))
     last_name = Column(String(length=50))
-    nickname = Column(String(length=50))
     hashed_password = Column(String(length=100))
