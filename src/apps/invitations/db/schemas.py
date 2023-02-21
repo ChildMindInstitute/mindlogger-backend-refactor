4265a627e0572779653f74c9bd31d513f92b3d15
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-02-21 12:37:46 +0200

    Provide better code quality

--- src/apps/invitations/db/schemas.py
+++ src/apps/invitations/db/schemas.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, String, Text
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 
 from infrastructure.database import Base
