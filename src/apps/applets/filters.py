d15cf20e4434b533f4f38e8f71ed46cefc36af4f
tom <isoqov12@gmail.com>
2023-03-13 16:13:50 +0500

    m2-769, change Applet query params

--- src/apps/applets/filters.py
+++ src/apps/applets/filters.py
@@ -1,8 +1,10 @@
+import uuid
+
 from apps.applets.domain import Role
 from apps.shared.query_params import BaseQueryParams
 
 
 class AppletQueryParams(BaseQueryParams):
-    owner_id: int | None
+    owner_id: uuid.UUID | None
     roles: str = ",".join(Role.as_list())
     ordering: str = "-id"
