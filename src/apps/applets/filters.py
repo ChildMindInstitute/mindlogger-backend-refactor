07a42ca6ee87676b0179181e72a866ade774101c
Tulkinjon Isakov <isoqov12@gmail.com>
2023-03-22 16:11:48 +0500

    m2-970, remove redundant filter column (#143)
    * m2-970, remove redundant filter column
    
    * m2-970, remove redundant filter column

--- src/apps/applets/filters.py
+++ src/apps/applets/filters.py
@@ -1,11 +1,8 @@
-import uuid
-
 from apps.applets.domain import Role
 from apps.shared.query_params import BaseQueryParams
 
 
 class AppletQueryParams(BaseQueryParams):
-    owner_id: uuid.UUID | None
     roles: str = ",".join(Role.as_list())
     ordering: str = "-id"
     folder_id: str | None = None
