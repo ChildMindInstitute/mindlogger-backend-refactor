51c32c49507ab3ade1ae7366bda46fa7aed76744
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-03-23 18:15:09 +0200

    Provide better code quality

--- src/infrastructure/database/migrations/versions/2023_03_23_15_03-.py
+++ src/infrastructure/database/migrations/versions/2023_03_23_15_03-.py
@@ -5,9 +5,8 @@ Revises: 1adbcf5d00f5
 Create Date: 2023-03-23 15:03:35.117086
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "ffb241cb1926"
