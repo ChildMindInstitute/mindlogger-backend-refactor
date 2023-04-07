262bd19481cf86254b1fd4250006df2d9b71699b
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-04-07 07:40:25 +0300

    Users migration

--- Makefile
+++ Makefile
@@ -19,6 +19,11 @@ DOCKER_EXEC = docker-compose exec fastapi
 run:
 	uvicorn src.main:app --proxy-headers --host ${HOST} --port ${PORT} --reload
 
+# NOTE: This command is used to run migration from Mongo to Postgres
+.PHONY: run_migration_mongo_to_postgres
+run_migration_mongo_to_postgres:
+	uvicorn src.apps.migrate.mongo_to_psql:app --proxy-headers --host 0.0.0.0 --port 8080
+
 .PHONY: run_local
 run_local:
 	docker-compose up -d redis postgres mailhog
--- src/apps/migrate/mongo_to_psql.py
+++ src/apps/migrate/mongo_to_psql.py
@@ -1,3 +1,7 @@
+import uuid
+from datetime import datetime
+
+import psycopg2  # type: ignore[import]
 from fastapi import FastAPI
 from pymongo import MongoClient
 
@@ -19,21 +23,78 @@ def create_app():
     db = client[db_name]
 
     collection = db["user"]
-    # documents = collection.find()
-    # for document in documents:
-    #     print(document)
+    users = collection.find(
+        {}, {"email": 1, "firstName": 1, "lastName": 1, "salt": 1}
+    ).limit(5)
+    count = 0
+    users_list = []
+    for user in users:
+        count += 1
 
-    user = collection.find_one(
-        {"email": "qwer@fn.com"}, {"_id": 1, "email": 1, "firstName": 1}
+        # If you want to decrypt values during migration
+        # you need the AES_KEY.
+        # And you should use something similar to this:
+        # pipenv install --dev pycryptodomex == 3.9.7
+        # from Cryptodome.Cipher import AES
+        #
+        # def decrypt(self, data):
+        #     try:
+        #         cipher = AES.new(
+        #               self.AES_KEY, AES.MODE_EAX, nonce=data[-32:-16]
+        #         )
+        #         plaintext = cipher.decrypt(data[:-32])
+        #         cipher.verify(data[-16:])
+        #
+        #         txt = plaintext.decode('utf-8')
+        #         length = int(txt[-self.maxCount:])
+        #
+        #         return ('ok', txt[:length])
+        #     except:
+        #         return ('error', None)
+
+        users_list.append(
+            {
+                "created_at": datetime.now(),
+                "updated_at": datetime.now(),
+                "is_deleted": False,
+                "email": user.get("email"),
+                "hashed_password": user.get("salt"),
+                "id": uuid.uuid4(),
+                "first_name": user.get("firstName"),
+                "last_name": user.get("lastName"),
+                "last_seen_at": datetime.now(),
+            }
+        )
+
+    # Close Mongodb connection
+    client.close()
+
+    # Connection to Postgresql
+    conn = psycopg2.connect(
+        host="localhost",
+        dbname="mindlogger_backend",
+        user="postgres",
+        password="postgres",
     )
 
-    print("user", user)
-    print("user.get('email')", user.get("email"))
-    print("user.get('firstName')", user.get("firstName"))
-    first_name = user.get("firstName")
-    print("first_name", first_name)
-    # Close connection
-    client.close()
+    cursor = conn.cursor()
+
+    for user in users_list:
+        cursor.execute(
+            "INSERT INTO users"
+            "(created_at, updated_at, is_deleted, email, hashed_password, "
+            "id, first_name, last_name, last_seen_at)"
+            "VALUES"
+            f"('{user['created_at']}', '{user['updated_at']}', "
+            f"'{user['is_deleted']}', '{user['email']}', "
+            f"'{user['hashed_password']}', '{user['id']}', "
+            f"'{user['first_name']}', '{user['last_name']}', "
+            f"'{user['last_seen_at']}');"
+        )
+
+    conn.commit()
+    cursor.close()
+    conn.close()
 
     return _app
 
