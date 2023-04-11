09e6474c98f65b54dd269170cfac83f239e42c42
ushviachko <ushviachko@scnsoft.com>
2023-04-11 21:16:45 +0300

    M2-768 Implement activity flow for reproschema protocol

--- src/apps/jsonld_converter/service/document/__init__.py
+++ src/apps/jsonld_converter/service/document/__init__.py
@@ -16,4 +16,5 @@ from apps.jsonld_converter.service.document.field import (
     ReproFieldAudioStimulus,
 )
 from apps.jsonld_converter.service.document.activity import ReproActivity  # noqa: F401, F403
+from apps.jsonld_converter.service.document.activity_flow import ReproActivityFlow  # noqa: F401, F403
 from apps.jsonld_converter.service.document.protocol import ReproProtocol  # noqa: F401, F403
