f7037022d843c95e356805310972b52668b301b0
tom <isoqov12@gmail.com>
2023-04-13 15:36:50 +0500

    m2-1131, update swagger

--- src/apps/answers/router.py
+++ src/apps/answers/router.py
@@ -10,9 +10,10 @@ from apps.answers.api import (
 )
 from apps.answers.domain import (
     ActivityAnswerPublic,
-    PublicAnsweredAppletActivity,
+    PublicAnsweredAppletActivity, AnswerNoteDetailPublic,
 )
-from apps.shared.domain import Response, ResponseMulti
+from apps.shared.domain import Response, ResponseMulti, \
+    AUTHENTICATION_ERROR_RESPONSES
 from apps.shared.domain.response import DEFAULT_OPENAPI_RESPONSE
 
 router = APIRouter(prefix="/answers", tags=["Answers"])
@@ -23,6 +24,7 @@ router.post(
     status_code=status.HTTP_201_CREATED,
     responses={
         **DEFAULT_OPENAPI_RESPONSE,
+        **AUTHENTICATION_ERROR_RESPONSES,
     },
 )(create_answer)
 
@@ -32,9 +34,9 @@ router.get(
     response_model=ResponseMulti[PublicAnsweredAppletActivity],
     responses={
         **DEFAULT_OPENAPI_RESPONSE,
+        **AUTHENTICATION_ERROR_RESPONSES,
     },
 )(applet_activities_list)
-
 
 router.get(
     "/applet/{applet_id}/answers/{answer_id}",
@@ -42,6 +44,7 @@ router.get(
     response_model=Response[ActivityAnswerPublic],
     responses={
         **DEFAULT_OPENAPI_RESPONSE,
+        **AUTHENTICATION_ERROR_RESPONSES,
     },
 )(applet_answer_retrieve)
 
@@ -50,13 +53,16 @@ router.post(
     status_code=status.HTTP_201_CREATED,
     responses={
         **DEFAULT_OPENAPI_RESPONSE,
+        **AUTHENTICATION_ERROR_RESPONSES,
     },
 )(note_add)
 
 router.get(
     "/applet/{applet_id}/answers/{answer_id}/notes",
     status_code=status.HTTP_200_OK,
+    response_model=ResponseMulti[AnswerNoteDetailPublic],
     responses={
         **DEFAULT_OPENAPI_RESPONSE,
+        **AUTHENTICATION_ERROR_RESPONSES,
     },
 )(note_list)
