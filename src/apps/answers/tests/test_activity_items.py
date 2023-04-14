5fc31eefdb3f390c4984acdbad5cf1474ec5adcf
Tulkinjon Isakov <isoqov12@gmail.com>
2023-04-14 15:03:51 +0500

    add test to create flow answers (#190)

--- src/apps/answers/tests/test_activity_items.py
+++ src/apps/answers/tests/test_activity_items.py
@@ -1,5 +1,7 @@
 import datetime
 
+import pytest
+
 from apps.shared.test import BaseTest
 from infrastructure.database import rollback
 
@@ -12,10 +14,13 @@ class TestAnswerActivityItems(BaseTest):
         "applets/fixtures/applet_user_accesses.json",
         "applets/fixtures/applet_histories.json",
         "activities/fixtures/activities.json",
-        "activities/fixtures/activity_histories.json",
         "activities/fixtures/activity_items.json",
-        "activities/fixtures/activity_item_histories.json",
         "activity_flows/fixtures/activity_flows.json",
+        "activity_flows/fixtures/activity_flow_items.json",
+        "activities/fixtures/activity_histories.json",
+        "activities/fixtures/activity_item_histories.json",
+        "activity_flows/fixtures/activity_flow_histories.json",
+        "activity_flows/fixtures/activity_flow_item_histories.json",
     ]
 
     login_url = "/auth/login"
@@ -36,6 +41,42 @@ class TestAnswerActivityItems(BaseTest):
         create_data = dict(
             applet_id="92917a56-d586-4613-b7aa-991f2c4b15b1",
             version="1.0.0",
+            activity_id="09e3dbf0-aefb-4d0e-9177-bdb321bf3611",
+            created_at=1681216969,
+            answers=[
+                dict(
+                    activity_item_id="a18d3409-2c96-4a5e-a1f3-1c1c14be0011",
+                    answer=dict(
+                        value="2ba4bb83-ed1c-4140-a225-c2c9b4db66d2",
+                        additional_text=None,
+                    ),
+                ),
+                dict(
+                    activity_item_id="a18d3409-2c96-4a5e-a1f3-1c1c14be0014",
+                    answer=dict(
+                        value="string",
+                    ),
+                ),
+            ],
+        )
+
+        response = await self.client.post(
+            self.answer_activity_item_create_url, data=create_data
+        )
+
+        assert response.status_code == 201, response.json()
+
+    @pytest.mark.main
+    @rollback
+    async def test_answer_flow_items_create_for_respondent(self):
+        await self.client.login(
+            self.login_url, "tom@mindlogger.com", "Test1234!"
+        )
+
+        create_data = dict(
+            applet_id="92917a56-d586-4613-b7aa-991f2c4b15b1",
+            version="1.0.0",
+            flow_id="3013dfb1-9202-4577-80f2-ba7450fb5831",
             activity_id="09e3dbf0-aefb-4d0e-9177-bdb321bf3611",
             created_at=1681216969,
             answers=[
