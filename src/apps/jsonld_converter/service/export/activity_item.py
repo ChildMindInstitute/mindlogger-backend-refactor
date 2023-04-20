ff582ffafdf024c060a93c863b2629be8a814c5e
ushviachko <ushviachko@scnsoft.com>
2023-04-20 14:48:20 +0300

    M2-768 M2-855 Implement Slider export

--- src/apps/jsonld_converter/service/export/activity_item.py
+++ src/apps/jsonld_converter/service/export/activity_item.py
@@ -6,8 +6,13 @@ from apps.activities.domain.activity_full import (
 from apps.activities.domain.response_type_config import (
     ResponseType,
     SingleSelectionConfig,
+    SliderConfig,
+    TextConfig,
 )
-from apps.activities.domain.response_values import SingleSelectionValues
+from apps.activities.domain.response_values import (
+    SingleSelectionValues,
+    SliderValues,
+)
 from apps.jsonld_converter.service.base import LdKeyword
 from apps.jsonld_converter.service.export.base import (
     BaseModelExport,
@@ -40,6 +45,7 @@ class ActivityItemBaseExport(BaseModelExport):
             "skos:prefLabel": model.name,
             "skos:altLabel": model.name,
             "question": model.question,
+            "allowEdit": True,
             "ui": self._build_ui_prop(model)
         }
         response_options = self._build_response_options_prop(model)
@@ -92,15 +98,16 @@ class ActivityItemTextExport(ActivityItemBaseExport):
         return allow
 
     def _build_response_options_prop(self, model: ActivityItemFull) -> dict | None:
+        config: TextConfig = model.config
         value_type = "xsd:string"
-        if model.config.numerical_response_required:
+        if config.numerical_response_required:
             value_type = "xsd:integer"
         options = {
             "valueType": value_type,
-            "removeBackOption": model.config.remove_back_button,
-            "requiredValue": model.config.response_required,
-            "isResponseIdentifier": model.config.response_data_identifier,
-            "maxLength": model.config.max_response_length
+            "removeBackOption": config.remove_back_button,
+            "requiredValue": config.response_required,
+            "isResponseIdentifier": config.response_data_identifier,
+            "maxLength": config.max_response_length
         }
 
         return options
@@ -180,3 +187,81 @@ class ActivityItemMultipleSelectExport(ActivityItemSingleSelectExport):
         options = super()._build_response_options_prop(model)
         options["multipleChoice"] = True
         return options
+
+
+class ActivityItemSliderExport(ActivityItemBaseExport):
+    @classmethod
+    def _get_supported_response_type(cls) -> ResponseType:
+        return ResponseType.SLIDER
+
+    def _get_input_type(self):
+        return 'slider'
+
+    def _build_doc(self, model: ActivityItemFull) -> dict:
+        doc = super()._build_doc(model)
+        additional_option = model.config.additional_response_option
+        if additional_option:
+            doc["isOptionalText"] = additional_option.text_input_option
+
+        doc.update({
+            "timer": model.config.timer * 1000  # set in milliseconds
+        })
+
+        return doc
+
+    def _build_allow_prop(self, model: ActivityItemFull) -> list[str]:
+        allow = super()._build_allow_prop(model)
+        if model.config.skippable_item:
+            allow.append("dontKnow")
+        return allow
+
+    def _build_response_options_prop(self, model: ActivityItemFull) -> dict | None:
+        config: SliderConfig = model.config
+        values: SliderValues = model.response_values
+
+        options = {
+            "valueType": "xsd:integer",
+            "removeBackOption": config.remove_back_button,
+            "scoring": config.add_scores,
+            "responseAlert": config.set_alerts,
+            "choices": self._build_choices_prop(model),
+            "continousSlider": config.continuous_slider,
+            "tickLabel": config.show_tick_labels,
+            # "textAnchors": true,  # TODO
+            "tickMark": config.show_tick_marks,
+            "schema:minValue": values.min_label,
+            "schema:maxValue": values.max_label,
+            "schema:minValueImg": values.min_image,
+            "schema:maxValueImg": values.max_image,
+        }
+        additional_option = config.additional_response_option
+        if additional_option and additional_option.text_input_option:
+            options["isOptionalTextRequired"] = additional_option.text_input_required
+
+        return options
+
+    def _build_choices_prop(self, model: ActivityItemFull) -> list:
+        choices = []
+        values: SliderValues = model.response_values
+        scores = values.scores
+        for i, v in enumerate(range(values.min_value, values.max_value + 1)):
+            lbl = str(v)
+            image = None
+            if v == values.min_value:
+                lbl = values.min_label
+                image = values.min_image
+            elif v == values.max_value:
+                lbl = values.max_label
+                image = values.max_image
+            choice = {
+                "schema:name": lbl,
+                "schema:value": v,
+            }
+            if scores:
+                choice["schema:score"] = scores[i]
+            if image:
+                choice["schema:image"] = image
+
+            choices.append(choice)
+
+        return choices
