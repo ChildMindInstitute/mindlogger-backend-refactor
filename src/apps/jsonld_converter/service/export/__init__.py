a4f50980607ef30e4ab2db6e0984fcd2063dc2dd
ushviachko <ushviachko@scnsoft.com>
2023-04-20 17:42:42 +0300

    M2-768 M2-855 Implement Audio, Photo, Video, Drawing, AudioPlayer export

--- src/apps/jsonld_converter/service/export/__init__.py
+++ src/apps/jsonld_converter/service/export/__init__.py
@@ -8,6 +8,11 @@ from apps.jsonld_converter.service.export.activity_item import (  # noqa: F401, 
     ActivityItemDateExport,
     ActivityItemTimeRangeExport,
     ActivityItemGeolocationExport,
+    ActivityItemAudioExport,
+    ActivityItemPhotoExport,
+    ActivityItemVideoExport,
+    ActivityItemDrawingExport,
+    ActivityItemAudioPlayerExport,
 )
 from apps.jsonld_converter.service.export.activity import ActivityExport  # noqa: F401, F403
 from apps.jsonld_converter.service.export.activity_flow import ActivityFlowExport  # noqa: F401, F403
