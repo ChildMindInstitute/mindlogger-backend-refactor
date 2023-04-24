42576fde833107ded7a4a9e556ce4e5f766f92fc
Tulkinjon Isakov <isoqov12@gmail.com>
2023-04-24 15:06:26 +0500

    m2-1543, clear invitations for transferred applet (#223)
    * m2-1543, clear invitations for transferred applet

--- src/apps/invitations/crud.py
+++ src/apps/invitations/crud.py
@@ -1,7 +1,7 @@
 import uuid
 from typing import Any
 
-from sqlalchemy import select, update
+from sqlalchemy import delete, select, update
 from sqlalchemy.engine import Result
 from sqlalchemy.orm import Query
 from sqlalchemy.sql.functions import count
@@ -300,4 +300,9 @@ class InvitationCRUD(BaseCRUD[InvitationSchema]):
         query = query.where(InvitationSchema.id == id_)
         query = query.values(status=InvitationStatus.DECLINED)
 
+        await self._execute(query)
+
+    async def delete_by_applet_id(self, applet_id: uuid.UUID):
+        query: Query = delete(InvitationSchema)
+        query = query.where(InvitationSchema.applet_id == applet_id)
         await self._execute(query)
