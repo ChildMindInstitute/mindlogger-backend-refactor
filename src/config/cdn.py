74df8b4b6951564766a808af51360f05e4ebde7f
Tulkinjon Isakov <isoqov12@gmail.com>
2023-04-24 18:14:58 +0500

    add testing cdn credentials (#226)

--- src/config/cdn.py
+++ src/config/cdn.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class CDNSettings(BaseModel):
     """Configure cdn settings"""
 
-    secret_key: str = ""
-    access_key: str = ""
-    region: str = ""
-    bucket: str = ""
+    secret_key: str = "0Bdiw4ajymm9FD4752b6PEL962CsjPpMTru06oFy"
+    access_key: str = "AKIAVGNWTLO55ZUXJN5I"
+    region: str = "us-east-1"
+    bucket: str = "tom12-test-bucket"
