5065ba469f568d55eba35c94acd44a0c6492ca36
Tulkinjon Isakov <isoqov12@gmail.com>
2023-05-04 11:29:41 +0500

    remove exception middleware, add internalization middleware (#269)

--- src/middlewares/__init__.py
+++ src/middlewares/__init__.py
@@ -1,3 +1,3 @@
 from middlewares.cors import *  # noqa: F401, F403
 from middlewares.domain import *  # noqa: F401, F403
-from middlewares.exception import *  # noqa: F401, F403
+from middlewares.internalization import *  # noqa: F401, F403
