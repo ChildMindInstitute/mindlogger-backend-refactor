d562db7b033b70fbaf5d762bbb7f6c7e583cad0f
Damirkhon Aloev <aloevdamirkhon@gmail.com>
2023-05-17 13:40:18 +0500

    m2-1822:contracts for score and report

--- src/apps/activities/domain/activity_base.py
+++ src/apps/activities/domain/activity_base.py
@@ -3,6 +3,32 @@ from pydantic import BaseModel, Field
 from apps.shared.enums import Language
 
 
+class Score(BaseModel):
+    score_name: str
+    score_id: str
+    calculation_type: str
+    range_of_scores: str
+    items: list[str] | None = Field(default_factory=list)
+    show_message: bool = False
+    message: str = ""
+    print_items: bool = False
+
+
+class Section(BaseModel):
+    pass
+
+
+class ScoresAndReports(BaseModel):
+    generate_report: bool = False
+    show_score_summary: bool = False
+    scores: list[Score] | None = Field(default_factory=list)
+    sections: list[Section] | None = Field(default_factory=list)
+
+
+class Subscale(BaseModel):
+    pass
+
+
 class ActivityBase(BaseModel):
     name: str
     description: dict[Language, str] = Field(default_factory=dict)
@@ -13,3 +39,5 @@ class ActivityBase(BaseModel):
     is_reviewable: bool = False
     response_is_editable: bool = False
     is_hidden: bool = False
+    scores_and_reports: ScoresAndReports | None = None
+    subscales: list[Subscale] | None = Field(default_factory=list)
