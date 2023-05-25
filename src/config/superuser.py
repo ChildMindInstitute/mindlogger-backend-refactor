08bca666a1a552864bf50fcba741f7d857f0e560
Tulkinjon Isakov <isoqov12@gmail.com>
2023-05-25 16:11:07 +0500

    m2-1989, add super admin, change permission checking for publish and conceal (#332)

---
+++ src/config/superuser.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class SuperAdmin(BaseModel):
+    email: str = "admin@mindlogger.com"
+    password: str = "superAdminPassword!"
+    first_name: str = "Mindlogger"
+    last_name: str = "ChildMindInstitute"
+    force_update: bool = False
