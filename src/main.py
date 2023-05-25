47e2d9b0fef06209630fd418c78b548d219323a8
tom <isoqov12@gmail.com>
2023-05-25 20:49:25 +0500

    add startup event

--- src/main.py
+++ src/main.py
@@ -3,13 +3,13 @@ from infrastructure.app import create_app
 app = create_app()
 
 
-# @app.on_event("startup")
-# async def create_superuser():
-#     print("Create/Update superuser")
-#     from apps.users.services.user import UserService
-#     from infrastructure.database import atomic, session_manager
-#
-#     session = session_manager.get_session()
-#
-#     async with atomic(session):
-#         await UserService(session).create_superuser()
+@app.on_event("startup")
+async def create_superuser():
+    print("Create/Update superuser")
+    from apps.users.services.user import UserService
+    from infrastructure.database import atomic, session_manager
+
+    session = session_manager.get_session()
+
+    async with atomic(session):
+        await UserService(session).create_superuser()
