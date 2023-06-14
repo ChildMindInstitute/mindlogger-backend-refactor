9adac029dd06d641466c7dd29eec167391c2986d
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-06-14 16:19:19 +0300

    Alerts fixed

--- src/apps/alerts/api/alert.py
+++ src/apps/alerts/api/alert.py
@@ -1,3 +1,4 @@
+import asyncio
 import uuid
 from copy import deepcopy
 from time import sleep
@@ -13,12 +14,13 @@ from apps.alerts.errors import AlertUpdateAccessDenied, AlertViewAccessDenied
 from apps.alerts.filters import AlertConfigQueryParams
 from apps.alerts.service.alert import AlertService
 from apps.answers.domain import AppletAnswerCreate
-from apps.authentication.deps import get_current_user
+from apps.authentication.deps import get_current_user, get_current_user_for_ws
 from apps.shared.domain import Response, ResponseMulti
 from apps.shared.query_params import QueryParams, parse_query_params
 from apps.users.domain import User
 from apps.workspaces.crud.user_applet_access import UserAppletAccessCRUD
 from apps.workspaces.domain.constants import Role
+from config import settings
 from infrastructure.database import atomic
 from infrastructure.database.deps import get_session
 
@@ -40,6 +42,7 @@ async def alerts_create(
             raise AlertViewAccessDenied
 
         await AlertService(session, user.id).create_alert(applet_answer)
+
         return
 
 
@@ -125,3 +128,37 @@ async def alert_update_status_by_id(
     alert = Alert(**instance.dict())
 
     return Response(result=alert)
+
+
+async def ws_alert_get_all_by_applet_id(
+    websocket: WebSocket,
+    applet_id: uuid.UUID,
+    user: User = Depends(get_current_user_for_ws),
+    session=Depends(get_session),
+):
+    await websocket.accept()
+
+    while True:
+        async with atomic(session):
+            roles = await UserAppletAccessCRUD(session).get_roles_in_roles(
+                user.id,
+                applet_id,
+                [Role.OWNER],
+            )
+            if not roles:
+                raise AlertViewAccessDenied
+
+            # Get all alert for specific applet
+            instances = await AlertCRUD(session).get_by_applet_id(applet_id)
+            count = await AlertCRUD(session).get_by_applet_id_count(applet_id)
+
+        result = ResponseMulti(
+            result=[
+                AlertPublic.from_orm(alert_config)
+                for alert_config in instances
+            ],
+            count=count,
+        )
+
+        await websocket.send_json(result.json())
+        await asyncio.sleep(settings.alerts.ws_fetching_periodicity_sec)
