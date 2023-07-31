7c47469867da00ff7b3ae44db492f777c633a455
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-07-31 15:15:53 +0300

    Removed email_hash from users

--- src/apps/users/db/schemas.py
+++ src/apps/users/db/schemas.py
@@ -15,8 +15,7 @@ from infrastructure.database.base import Base
 class UserSchema(Base):
     __tablename__ = "users"
 
-    email = Column(String(length=100), unique=True)
-    email_hash = Column(String(length=56), unique=True)
+    email = Column(String(length=56), unique=True)
     email_aes_encrypted = Column(LargeBinary(length=100), default=None)
     first_name = Column(String(length=50))
     last_name = Column(String(length=50))
