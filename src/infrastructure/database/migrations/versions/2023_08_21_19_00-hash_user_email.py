696683a2b2d19d52bbe5cb725a320a0830577339
karser <dpoddubny@gmail.com>
2023-08-21 14:57:32 +0300

    convert users emails to hash

---
+++ src/infrastructure/database/migrations/versions/2023_08_21_19_00-hash_user_email.py
@@ -0,0 +1,24 @@
+"""hash user email
+
+Revision ID: 879e5b56ca8b
+Revises: 956eafa172cb
+Create Date: 2023-08-21 19:00:48.767562
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "879e5b56ca8b"
+down_revision = "956eafa172cb"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("""
+        update users set email=encode(sha224('email'::bytea), 'hex') where email like '%@%';
+    """)
+
+
+def downgrade() -> None:
+    return
