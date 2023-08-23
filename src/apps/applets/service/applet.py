140e10df9fa0461e61cebbdfdca1c8440d876bef
Ivan Koryshkin <ivan.koryshkin@gmail.com>
2023-08-23 14:21:37 +0400

    m2-3031: remove report_email_body

--- src/apps/applets/service/applet.py
+++ src/apps/applets/service/applet.py
@@ -316,7 +316,6 @@ class AppletService:
             report_public_key=applet_exist.report_public_key,
             report_include_user_id=applet_exist.report_include_user_id,
             report_include_case_id=applet_exist.report_include_case_id,
-            report_email_body=applet_exist.report_email_body,
         )
 
     async def _validate_applet_name(
