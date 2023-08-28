80c27cfe07c663871c301b90af5417b4f9ff767f
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-08-28 16:01:01 +0300

    Changed logic

--- src/apps/workspaces/errors.py
+++ src/apps/workspaces/errors.py
@@ -14,6 +14,7 @@ __all__ = [
     "UserAppletAccessesDenied",
     "AccessDeniedToUpdateOwnAccesses",
     "RemoveOwnPermissionAccessDenied",
+    "UserAccessAlreadyExists",
 ]
 
 
--- src/apps/workspaces/service/user_access.py
+++ src/apps/workspaces/service/user_access.py
@@ -397,12 +397,16 @@ class UserAccessService:
             ).get_by_user_applet_accesses(
                 schema.user_id, schema.applet_id, schema.role
             )
-            if user_access and not user_access.is_deleted:
-                raise UserAccessAlreadyExists()
-            else:
+            if not user_access:
                 await UserAppletAccessCRUD(
                     self.session
                 ).upsert_user_applet_access(schema)
+            elif user_access.is_deleted:
+                await UserAppletAccessCRUD(
+                    self.session
+                ).upsert_user_applet_access(schema)
+            else:
+                raise UserAccessAlreadyExists()
 
     async def get_workspace_applet_roles(
         self,
