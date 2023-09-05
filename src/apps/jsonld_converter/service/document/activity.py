a742745199e562a216c9b2c78c75af00e0afe27c
karser <dpoddubny@gmail.com>
2023-09-05 15:38:19 +0300

    M2-3195 fix conditional logic contains full url while the item id is short

--- src/apps/jsonld_converter/service/document/activity.py
+++ src/apps/jsonld_converter/service/document/activity.py
@@ -260,6 +260,16 @@ class ReproActivity(LdDocumentBase, ContainsNestedMixin, CommonFieldsMixin):
                 for prop, val in self.properties[node.ld_id].items():
                     if val is not None and hasattr(node, prop):
                         setattr(node, prop, val)
+            else:
+                # in case conditional logic contains full url while the item id is short
+                # e.g https://github.com/a/b/item_name and item.id is item_name
+                for _prop, _val in self.properties.items():
+                    if node.ld_id == _prop.rsplit('/', 1).pop():
+                        for prop, val in _val.items():
+                            if val is not None and hasattr(node, prop):
+                                setattr(node, prop, val)
+                        break
+
             return node
         except JsonLDNotSupportedError:
             if skip_not_supported:
