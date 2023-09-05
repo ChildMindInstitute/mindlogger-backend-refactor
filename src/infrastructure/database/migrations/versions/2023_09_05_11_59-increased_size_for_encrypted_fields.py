aa1df24805bee5cb5e22114a16e8bbba0a599113
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-09-05 13:52:52 +0300

    Fixed tests - not all

--- src/infrastructure/database/migrations/versions/2023_09_05_11_59-increased_size_for_encrypted_fields.py
+++ src/infrastructure/database/migrations/versions/2023_09_05_11_59-increased_size_for_encrypted_fields.py
@@ -1,15 +1,15 @@
-"""Encrypt fields
+"""Increased size for encrypted fields
 
-Revision ID: 6e52c8f842a1
+Revision ID: 6268b3e094a3
 Revises: a4b4299c90c5
-Create Date: 2023-09-04 11:41:54.030756
+Create Date: 2023-09-05 11:59:57.741477
 
 """
 import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "6e52c8f842a1"
+revision = "6268b3e094a3"
 down_revision = "a4b4299c90c5"
 branch_labels = None
 depends_on = None
