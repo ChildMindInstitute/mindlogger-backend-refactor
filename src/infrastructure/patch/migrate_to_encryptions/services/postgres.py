027b1345c88e73c8fb2df1d88dde2ae0e5129bb3
Dmytro Parfeniuk <dparfeniuk@scnsoft.com>
2023-09-06 12:45:26 +0300

    Added check if the field is encrypted

--- src/infrastructure/patch/migrate_to_encryptions/services/postgres.py
+++ src/infrastructure/patch/migrate_to_encryptions/services/postgres.py
@@ -2,7 +2,7 @@ import os
 
 import psycopg2
 
-from apps.shared.encryption import encrypt
+from apps.shared.encryption import decrypt, encrypt
 
 
 class Postgres:
@@ -22,145 +22,206 @@ class Postgres:
     def encrypt_users(self) -> None:
         """Encrypt users fields: first_name and last_name"""
         cursor = self.connection.cursor()
-        try:
-            cursor.execute("SELECT id, first_name, last_name FROM users;")
-            result = cursor.fetchall()
-            for row in result:
-                user_id, plain_first_name, plain_last_name = row
-                if plain_first_name:
+        # Encryption and updates users: first_name
+        cursor.execute(
+            "SELECT id, first_name FROM users " "WHERE first_name IS NOT NULL;"
+        )
+        result = cursor.fetchall()
+        count = 0
+        for row in result:
+            user_id, first_name = row
+            if first_name:
+                try:
+                    decrypt(bytes.fromhex(first_name)).decode("utf-8")
+                except ValueError:
                     encrypted_first_name = encrypt(
-                        bytes(plain_first_name, "utf-8")
+                        bytes(first_name, "utf-8")
                     ).hex()
-                else:
-                    encrypted_first_name = None
-                if plain_last_name:
+                    cursor.execute(
+                        "UPDATE users SET first_name = %s " "WHERE id = %s",
+                        (encrypted_first_name, user_id),
+                    )
+                    count += 1
+        self.connection.commit()
+        print(
+            "Users 'first_name' encryption and updates completed.\n"
+            f"{count} - Updated Successfully!"
+        )
+        # Encryption and updates users: last_name
+        cursor.execute(
+            "SELECT id, last_name FROM users " "WHERE last_name IS NOT NULL;"
+        )
+        result = cursor.fetchall()
+        count = 0
+        for row in result:
+            user_id, last_name = row
+            if last_name:
+                try:
+                    decrypt(bytes.fromhex(last_name)).decode("utf-8")
+                except ValueError:
                     encrypted_last_name = encrypt(
-                        bytes(plain_last_name, "utf-8")
+                        bytes(last_name, "utf-8")
                     ).hex()
-                else:
-                    encrypted_last_name = None
-                cursor.execute(
-                    "UPDATE users SET first_name = %s, last_name = %s "
-                    "WHERE id = %s",
-                    (encrypted_first_name, encrypted_last_name, user_id),
-                )
-        except Exception as e:
-            print(f"Users encryption and updates generate error: {e}.")
-        finally:
-            self.connection.commit()
-            cursor.close()
-
-        print("Users encryption and updates completed.")
+                    cursor.execute(
+                        "UPDATE users SET last_name = %s " "WHERE id = %s",
+                        (encrypted_last_name, user_id),
+                    )
+                    count += 1
+        self.connection.commit()
+        print(
+            "Users 'last_name' encryption and updates completed.\n"
+            f"{count} - Updated Successfully!"
+        )
+        cursor.close()
 
     def encrypt_users_workspaces(self) -> None:
         """Encrypt users_workspaces field: workspace_name"""
         cursor = self.connection.cursor()
-        try:
-            cursor.execute(
-                "SELECT id, workspace_name FROM users_workspaces "
-                "WHERE workspace_name IS NOT NULL;"
-            )
-            result = cursor.fetchall()
-            for row in result:
-                users_workspace_id, plain_workspace_name = row
-                encrypted_workspace_name = encrypt(
-                    bytes(plain_workspace_name, "utf-8")
-                ).hex()
-                cursor.execute(
-                    "UPDATE users_workspaces SET workspace_name = %s "
-                    "WHERE id = %s",
-                    (encrypted_workspace_name, users_workspace_id),
-                )
-        except Exception as e:
-            print(
-                f"Users_workspaces encryption and updates generate error: {e}."
-            )
-        finally:
-            self.connection.commit()
-            cursor.close()
-
-        print("Users_workspaces encryption and updates completed.")
+        # Encryption and updates users_workspaces: workspace_name
+        cursor.execute(
+            "SELECT id, workspace_name FROM users_workspaces "
+            "WHERE workspace_name IS NOT NULL;"
+        )
+        result = cursor.fetchall()
+        count = 0
+        for row in result:
+            users_workspace_id, workspace_name = row
+            if workspace_name:
+                try:
+                    decrypt(bytes.fromhex(workspace_name)).decode("utf-8")
+                except ValueError:
+                    encrypted_workspace_name = encrypt(
+                        bytes(workspace_name, "utf-8")
+                    ).hex()
+                    cursor.execute(
+                        "UPDATE users_workspaces SET workspace_name = %s "
+                        "WHERE id = %s",
+                        (encrypted_workspace_name, users_workspace_id),
+                    )
+                    count += 1
+        self.connection.commit()
+        print(
+            "Users_workspaces 'workspace_name' "
+            "encryption and updates completed.\n"
+            f"{count} - Updated Successfully!"
+        )
+        cursor.close()
 
     def encrypt_invitations(self) -> None:
         """Encrypt invitations fields: first_name and last_name"""
         cursor = self.connection.cursor()
-        try:
-            cursor.execute(
-                "SELECT id, first_name, last_name FROM invitations;"
-            )
-            result = cursor.fetchall()
-            for row in result:
-                invitation_id, plain_first_name, plain_last_name = row
-                if plain_first_name:
+        # Encryption and updates invitations: first_name
+        cursor.execute(
+            "SELECT id, first_name FROM invitations "
+            "WHERE first_name IS NOT NULL;"
+        )
+        result = cursor.fetchall()
+        count = 0
+        for row in result:
+            invitation_id, first_name = row
+            if first_name:
+                try:
+                    decrypt(bytes.fromhex(first_name)).decode("utf-8")
+                except ValueError:
                     encrypted_first_name = encrypt(
-                        bytes(plain_first_name, "utf-8")
+                        bytes(first_name, "utf-8")
                     ).hex()
-                else:
-                    encrypted_first_name = None
-                if plain_last_name:
+                    cursor.execute(
+                        "UPDATE invitations SET first_name = %s "
+                        "WHERE id = %s",
+                        (encrypted_first_name, invitation_id),
+                    )
+                    count += 1
+        self.connection.commit()
+        print(
+            "Invitations 'first_name' encryption and updates completed.\n"
+            f"{count} - Updated Successfully!"
+        )
+        # Encryption and updates invitations: last_name
+        cursor.execute(
+            "SELECT id, last_name FROM invitations "
+            "WHERE last_name IS NOT NULL;"
+        )
+        result = cursor.fetchall()
+        count = 0
+        for row in result:
+            invitation_id, last_name = row
+            if last_name:
+                try:
+                    decrypt(bytes.fromhex(last_name)).decode("utf-8")
+                except ValueError:
                     encrypted_last_name = encrypt(
-                        bytes(plain_last_name, "utf-8")
+                        bytes(last_name, "utf-8")
                     ).hex()
-                else:
-                    encrypted_last_name = None
-                cursor.execute(
-                    "UPDATE invitations SET first_name = %s, last_name = %s "
-                    "WHERE id = %s",
-                    (encrypted_first_name, encrypted_last_name, invitation_id),
-                )
-        except Exception as e:
-            print(f"Invitations encryption and updates generate error: {e}.")
-        finally:
-            self.connection.commit()
-            cursor.close()
-
-        print("Invitations encryption and updates completed.")
+                    cursor.execute(
+                        "UPDATE invitations SET last_name = %s "
+                        "WHERE id = %s",
+                        (encrypted_last_name, invitation_id),
+                    )
+                    count += 1
+        self.connection.commit()
+        print(
+            "Invitations 'last_name' encryption and updates completed.\n"
+            f"{count} - Updated Successfully!"
+        )
+        cursor.close()
 
     def encrypt_answer_notes(self) -> None:
         """Encrypt answer_notes field: note"""
         cursor = self.connection.cursor()
-        try:
-            cursor.execute(
-                "SELECT id, note FROM answer_notes WHERE note IS NOT NULL;"
-            )
-            result = cursor.fetchall()
-            for row in result:
-                answer_note_id, plain_note = row
-                encrypted_note = encrypt(bytes(plain_note, "utf-8")).hex()
-                cursor.execute(
-                    "UPDATE answer_notes SET note = %s WHERE id = %s",
-                    (encrypted_note, answer_note_id),
-                )
-        except Exception as e:
-            print(f"Answer_notes encryption and updates generate error: {e}.")
-        finally:
-            self.connection.commit()
-            cursor.close()
-
-        print("Answer_notes encryption and updates completed.")
+        # Encryption and updates answer_notes: note
+        cursor.execute(
+            "SELECT id, note FROM answer_notes " "WHERE note IS NOT NULL;"
+        )
+        result = cursor.fetchall()
+        count = 0
+        for row in result:
+            answer_note_id, note = row
+            if note:
+                try:
+                    decrypt(bytes.fromhex(note)).decode("utf-8")
+                except ValueError:
+                    encrypted_note = encrypt(bytes(note, "utf-8")).hex()
+                    cursor.execute(
+                        "UPDATE answer_notes SET note = %s " "WHERE id = %s",
+                        (encrypted_note, answer_note_id),
+                    )
+                    count += 1
+        self.connection.commit()
+        print(
+            "Answer_notes 'note' encryption and updates completed.\n"
+            f"{count} - Updated Successfully!"
+        )
+        cursor.close()
 
     def encrypt_alerts(self) -> None:
         """Encrypt alerts field: alert_message"""
         cursor = self.connection.cursor()
-        try:
-            cursor.execute(
-                "SELECT id, alert_message FROM alerts "
-                "WHERE alert_message IS NOT NULL;"
-            )
-            result = cursor.fetchall()
-            for row in result:
-                alert_id, plain_alert_message = row
-                encrypted_alert_message = encrypt(
-                    bytes(plain_alert_message, "utf-8")
-                ).hex()
-                cursor.execute(
-                    "UPDATE alerts SET alert_message = %s WHERE id = %s",
-                    (encrypted_alert_message, alert_id),
-                )
-        except Exception as e:
-            print(f"Alerts encryption and updates generate error: {e}.")
-        finally:
-            self.connection.commit()
-            cursor.close()
-
-        print("Alerts encryption and updates completed.")
+        # Encryption and updates alerts: alert_message
+        cursor.execute(
+            "SELECT id, alert_message FROM alerts "
+            "WHERE alert_message IS NOT NULL;"
+        )
+        result = cursor.fetchall()
+        count = 0
+        for row in result:
+            alerts_id, alert_message = row
+            if alert_message:
+                try:
+                    decrypt(bytes.fromhex(alert_message)).decode("utf-8")
+                except ValueError:
+                    encrypted_alert_message = encrypt(
+                        bytes(alert_message, "utf-8")
+                    ).hex()
+                    cursor.execute(
+                        "UPDATE alerts SET alert_message = %s "
+                        "WHERE id = %s",
+                        (encrypted_alert_message, alerts_id),
+                    )
+                    count += 1
+        self.connection.commit()
+        print(
+            "Alerts 'alert_message' encryption and updates completed.\n"
+            f"{count} - Updated Successfully!"
+        )
+        cursor.close()
