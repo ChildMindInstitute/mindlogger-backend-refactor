53ebab790ff7e79fd621c30a0314a17df62b9ef1
Yauheni Us <zenay.yss@gmail.com>
2023-12-20 03:08:58 +0300

    added improved logic for communication with LORIS

--- src/apps/integrations/loris/api/applets.py
+++ src/apps/integrations/loris/api/applets.py
@@ -1,5 +1,6 @@
 import time
 import uuid
+import json
 
 import aiohttp
 from fastapi import BackgroundTasks, Depends
@@ -8,11 +9,15 @@ from starlette.responses import Response as HTTPResponse
 
 from apps.answers.service import ReportServerService
 from apps.authentication.deps import get_current_user
-from apps.integrations.loris.domain import (  # UnencryptedApplet,
+from apps.integrations.loris.domain import (
+    UnencryptedApplet,
     LorisServerResponse,
 )
 from apps.integrations.loris.errors import LorisServerError
 from apps.users.domain import User
+from apps.activities.crud.activity import ActivitiesCRUD
+from apps.activities.crud.activity_item import ActivityItemsCRUD
+from apps.applets.crud.applets import AppletsCRUD
 from infrastructure.database.deps import get_session
 from infrastructure.logger import logger
 
@@ -22,24 +27,102 @@ __all__ = [
 
 
 # TODO move to env and config
-LORIS_URL = "localhost/mindlogger/applet_data"
+LORIS_LOGIN_URL = "https://loris.cmiml.net/api/v0.0.3/login/"
+LORIS_ML_URL = "https://loris.cmiml.net/mindlogger/v1/schema/"
+
+LORIS_USERNAME = "lorisfrontadmin"
+LORIS_PASSWORD = "2eN5i4gmbWpDQb08"
+
+LORIS_LOGIN_DATA = {
+    "username": LORIS_USERNAME,
+    "password": LORIS_PASSWORD,
+}
 
 
 async def integration(applet_id: uuid.UUID, session):
-    print("start backgroud task")
-    time.sleep(10)
-    print(f"end backgroud task. applet id is: {applet_id}")
-
     report_service = ReportServerService(session)
+    decrypted_answers = await report_service.decrypt_data_for_loris(applet_id)
 
-    schema = await report_service.decrypt_data_for_loris(applet_id)
+    activities_crud = ActivitiesCRUD(session)
+    activities_items_crud = ActivityItemsCRUD(session)
+    applet_crud = AppletsCRUD(session)
 
-    async with aiohttp.ClientSession() as session:
-        logger.info(f"Sending request to the loris server {LORIS_URL}.")
+    applet = await applet_crud.get_by_id(applet_id)
+
+    loris_data = {
+        "id": applet_id,
+        "displayName": applet.display_name,
+        "description": list(applet.description.values())[0],
+        "activities": None,
+    }
+    activities: list = []
+    for activitie in decrypted_answers["result"]:
+        items: list = []
+        act_id = activitie["activityId"]
+        _activitie = await activities_crud.get_by_applet_id_and_activity_id(
+            applet_id, uuid.UUID(act_id)
+        )
+        _activities_items = await activities_items_crud.get_by_activity_id(
+            uuid.UUID(act_id)
+        )
+        for item in _activities_items:
+            items.append(
+                {
+                    "id": item.id,
+                    "question": list(item.question.values())[0],
+                    "responseType": item.response_type,
+                    "responseValues": item.response_values,
+                    "config": item.config,
+                    "name": item.name,
+                    "isHidden": item.is_hidden,
+                    "conditionalLogic": item.conditional_logic,
+                    "allowEdit": item.allow_edit,
+                }
+            )
+        activities.append(
+            {
+                "id": _activitie.id,
+                "name": _activitie.name,
+                "description": list(_activitie.description.values())[0],
+                "splash_screen": _activitie.splash_screen,
+                "image": _activitie.image,
+                "order": _activitie.order,
+                "createdAt": _activitie.created_at,
+                "items": items,
+                # "results": activitie["data"],
+            }
+        )
+    loris_data["activities"] = activities
+    timeout = aiohttp.ClientTimeout(total=20)
+
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        logger.info(
+            f"Sending LOGIN request to the loris server {LORIS_LOGIN_URL}."
+        )
         start = time.time()
         async with session.post(
-            LORIS_URL,
-            json=dict(payload=schema),
+            LORIS_LOGIN_URL,
+            data=json.dumps(LORIS_LOGIN_DATA),
+        ) as resp:
+            duration = time.time() - start
+            if resp.status == 200:
+                logger.info(f"Successful request in {duration:.1f} seconds.")
+                response_data = await resp.json()
+                # return LorisServerResponse(**response_data)
+            else:
+                logger.error(f"Failed request in {duration:.1f} seconds.")
+                error_message = await resp.text()
+                raise LorisServerError(message=error_message)
+
+        logger.info(
+            f"Sending UPLOAD DATA request to the loris server {LORIS_ML_URL}."
+        )
+        headers = {"Authorization": f"Bearer: {response_data['token']}"}
+        start = time.time()
+        async with session.post(
+            LORIS_ML_URL,
+            data=UnencryptedApplet(**loris_data).json(),
+            headers=headers,
         ) as resp:
             duration = time.time() - start
             if resp.status == 200:
