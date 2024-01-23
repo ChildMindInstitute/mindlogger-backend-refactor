c477c1c09820b1efccf9a1370e1b9fbab13398e4
ivan koryshkin <ikoryshkin@scnsoft.com>
2024-01-23 19:01:18 +0400

    Merge alembic branches

---
+++ src/infrastructure/database/migrations/versions/2024_01_23_18_46-merge-subjects-into-main-migration-branch.py
@@ -0,0 +1,24 @@
+"""Merge subjects into main migration branch
+
+Revision ID: 7e19cfbcedbb
+Revises: 01115b529336, 46f285831ae8
+Create Date: 2024-01-23 18:46:24.103057
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7e19cfbcedbb"
+down_revision = ("01115b529336", "46f285831ae8")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
