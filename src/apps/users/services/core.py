7a5626be83abbac4d05d2bf303b6f2a2ac031db0
sultanofcardio <kenroy.gobourne@gmail.com>
2024-02-15 20:56:18 +0000

    M2-5203: Distinguish between web and admin for password reset email
    When a forgot password request comes in, the link in the reset email is always pointing to the respondent web app. This changes that to make use of the `Mindlogger-Content-Source` HTTP header to distinguish between the two and use the proper link as configured in the settings

--- src/apps/users/services/core.py
+++ src/apps/users/services/core.py
@@ -19,6 +19,7 @@ from apps.users.services import PasswordRecoveryCache
 from config import settings
 from infrastructure.cache import CacheNotFound
 from infrastructure.cache.domain import CacheEntry
+from infrastructure.http.domain import MindloggerContentSource
 
 __all__ = ["PasswordRecoveryService"]
 
@@ -28,7 +29,14 @@ class PasswordRecoveryService:
         self._cache: PasswordRecoveryCache = PasswordRecoveryCache()
         self.session = session
 
-    async def send_password_recovery(self, schema: PasswordRecoveryRequest) -> PublicUser:
+    async def send_password_recovery(
+        self,
+        schema: PasswordRecoveryRequest,
+        content_source: MindloggerContentSource,
+    ) -> PublicUser:
+
+        # encrypted_email = encrypt(bytes(schema.email, "utf-8")).hex()
+
         user: User = await UsersCRUD(self.session).get_by_email(schema.email)
 
         if user.email_encrypted != schema.email:
@@ -66,8 +74,18 @@ class PasswordRecoveryService:
 
         exp = settings.authentication.password_recover.expiration // 60
 
+        # Default to web frontend
+        frontend_base_url = settings.service.urls.frontend.web_base
+        if content_source == MindloggerContentSource.admin:
+            # Change to admin frontend if the request came from there
+            frontend_base_url = settings.service.urls.frontend.admin_base
+
+        base_url_with_protocol = frontend_base_url
+        if not base_url_with_protocol.startswith("http"):
+            base_url_with_protocol = f"https://{base_url_with_protocol}"
+
         url = (
-            f"https://{settings.service.urls.frontend.web_base}"
+            f"{base_url_with_protocol}"
             f"/{settings.service.urls.frontend.password_recovery_send}"
             f"?key={password_recovery_info.key}"
             f"&email="
