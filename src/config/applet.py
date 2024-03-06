dbd707ab9b3fa2dae88a5ab0a4f508d826693695
Ilya Bogretsov <35212515+ibogretsov@users.noreply.github.com>
2024-03-06 15:07:32 +0300

    M2-5602: Add command to export daily user flow schedule (#1153)
    * M2-5602: Add command to export daily user flow schedule

--- src/config/applet.py
+++ src/config/applet.py
@@ -6,3 +6,4 @@ class AppletEMASettings(BaseModel):
     name: str | None = None
     export_path_prefix: str = "export-ema"
     export_flow_file_name: str = "flow-items.csv"
+    export_user_flow_schedule_file_name: str = "{date}/daily-user-flow-schedule.csv"
