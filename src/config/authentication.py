3e8fc8dc43ed039b946c3bfdf3ae9a52d77e5522
Ilya Bogretsov <35212515+ibogretsov@users.noreply.github.com>
2024-03-20 18:51:10 +0300

    M2-4388: Remove secret_key and auth keys. Add local secret key to .env.example (#1176)

--- src/config/authentication.py
+++ src/config/authentication.py
@@ -1,24 +1,36 @@
-from pydantic import BaseModel
+from pydantic import BaseSettings, Field, validator
 
 
-class AccessTokenSettings(BaseModel):
-    secret_key: str = "e51bcf5f4cb8550ff3f6a8bb4dfe112a3da2cf5142929e1b281cd974c88fa66c"
+class AccessTokenSettings(BaseSettings):
+    secret_key: str = Field("", env="AUTHENTICATION__ACCESS_TOKEN__SECRET_KEY")
     # Set in minutes
     expiration: int = 30
 
+    @validator("secret_key")
+    def check_secret_key(cls, v: str) -> str:
+        if not v:
+            raise ValueError("Please specify AUTHENTICATION__ACCESS_TOKEN__SECRET_KEY variable")
+        return v
 
-class RefreshTokenSettings(BaseModel):
-    secret_key: str = "5da342d54ed5659f123cdd1cefe439c5aaf7e317a0aba1405375c07d32e097cc"
+
+class RefreshTokenSettings(BaseSettings):
+    secret_key: str = Field("", env="AUTHENTICATION__REFRESH_TOKEN__SECRET_KEY")
     # Set in minutes
     expiration: int = 540
 
+    @validator("secret_key")
+    def check_secret_key(cls, v: str) -> str:
+        if not v:
+            raise ValueError("Please specify AUTHENTICATION__REFRESH_TOKEN__SECRET_KEY variable")
+        return v
 
-class PasswordRecoverSettings(BaseModel):
+
+class PasswordRecoverSettings(BaseSettings):
     # Set in seconds
     expiration: int = 900
 
 
-class AuthenticationSettings(BaseModel):
+class AuthenticationSettings(BaseSettings):
     access_token: AccessTokenSettings = AccessTokenSettings()
     refresh_token: RefreshTokenSettings = RefreshTokenSettings()
     algorithm: str = "HS256"
