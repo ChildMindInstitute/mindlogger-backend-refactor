c2ec2036753f55c124cef199c3f415cf47e8ca59
ibahratsou <ibahratsou@scnsoft.com>
2024-03-29 13:09:12 +0300

    M2-6048: Add support for opentelemetry

--- src/infrastructure/app.py
+++ src/infrastructure/app.py
@@ -1,9 +1,15 @@
 from typing import Iterable, Type
 
+# Using the AWS resource Detectors
+import opentelemetry.trace as trace
 import sentry_sdk
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.routing import APIRouter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 import apps.activities.router as activities
 import apps.alerts.router as alerts
@@ -32,6 +38,24 @@ from infrastructure.http.execeptions import (
     python_base_error_handler,
 )
 from infrastructure.lifespan import shutdown, startup
+
+if settings.debug:
+    trace.set_tracer_provider(TracerProvider())
+    trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(OTLPSpanExporter()))  # type: ignore[attr-defined]
+    tracer = trace.get_tracer("mindlogger.tracer")
+else:
+    from opentelemetry.sdk.extension.aws.resource.ec2 import AwsEc2ResourceDetector
+    from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
+    from opentelemetry.sdk.resources import get_aggregated_resources
+
+    span_processor = BatchSpanProcessor(OTLPSpanExporter())
+    trace.set_tracer_provider(
+        TracerProvider(
+            active_span_processor=span_processor,  # type: ignore [arg-type]
+            id_generator=AwsXRayIdGenerator(),
+        )
+    )
+    trace.set_tracer_provider(TracerProvider(resource=get_aggregated_resources([AwsEc2ResourceDetector()])))
 
 # Declare your routers here
 routers: Iterable[APIRouter] = (
@@ -101,4 +125,5 @@ def create_app():
     app.add_exception_handler(BaseError, custom_base_errors_handler)
     app.add_exception_handler(Exception, python_base_error_handler)
 
+    FastAPIInstrumentor.instrument_app(app)
     return app
