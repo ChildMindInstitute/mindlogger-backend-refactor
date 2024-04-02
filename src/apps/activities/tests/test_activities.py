ca329dea66fe51ee03d03af89b28e8fef811aa06
Cleber Santos <cleber@metalab.com>
2024-04-02 00:27:50 -0300

    Add tests for `/activity/flows/applet/{applet_id}`

--- src/apps/activities/tests/test_activities.py
+++ src/apps/activities/tests/test_activities.py
@@ -29,6 +29,7 @@ class TestActivities:
     login_url = "/auth/login"
     activity_detail = "/activities/{pk}"
     activities_applet = "/activities/applet/{applet_id}"
+    activities_flows_applet = "/activities/flows/applet/{applet_id}"
     public_activity_detail = "public/activities/{pk}"
     answer_url = "/answers"
     applet_update_url = "applets/{applet_id}"
@@ -144,6 +145,94 @@ class TestActivities:
         assert result["appletDetail"]["activityFlows"] == []
 
         assert result["respondentMeta"] == {"nickname": f"{tom.first_name} {tom.last_name}"}
+
+    async def test_activities_flows_applet(
+        self, client, applet_activity_flow: AppletFull, default_theme: Theme, tom: User
+    ):
+        await client.login(self.login_url, "tom@mindlogger.com", "Test1234!")
+        response = await client.get(self.activities_flows_applet.format(applet_id=applet_activity_flow.id))
+
+        assert response.status_code == http.HTTPStatus.OK
+        result = response.json()["result"]
+
+        activity = applet_activity_flow.activities[0]
+        flow = applet_activity_flow.activity_flows[0]
+        assert len(result["details"]) == 2
+
+        assert result["details"][0]["id"] == str(activity.id)
+        assert result["details"][0]["name"] == activity.name
+        assert result["details"][0]["description"] == activity.description[Language.ENGLISH]
+        assert result["details"][0]["splashScreen"] == activity.splash_screen
+        assert result["details"][0]["image"] == activity.image
+        assert result["details"][0]["showAllAtOnce"] == activity.show_all_at_once
+        assert result["details"][0]["isSkippable"] == activity.is_skippable
+        assert result["details"][0]["isReviewable"] == activity.is_reviewable
+        assert result["details"][0]["isHidden"] == activity.is_hidden
+        assert result["details"][0]["responseIsEditable"] == activity.response_is_editable
+        assert result["details"][0]["order"] == 1
+        assert result["details"][0]["type"] == "activity"
+
+        items = result["details"][0]["items"]
+        activity_item = activity.items[0]
+        assert len(items) == 1
+        assert len(items) == len(activity.items)
+        item = items[0]
+        assert item["id"] == str(activity_item.id)
+        assert item["question"] == activity_item.question[Language.ENGLISH]
+        assert item["responseType"] == activity_item.response_type
+        assert item["name"] == activity_item.name
+        assert item["isHidden"] == activity_item.is_hidden
+        assert item["conditionalLogic"] == activity_item.conditional_logic
+        assert item["allowEdit"] == activity_item.allow_edit
+        assert len(item["responseValues"]["options"]) == 1
+        option = item["responseValues"]["options"][0]
+        activity_item.response_values = cast(SingleSelectionValues, activity_item.response_values)
+        assert item["responseValues"]["paletteName"] == activity_item.response_values.palette_name
+        activity_item_option = activity_item.response_values.options[0]
+        assert option["id"] == activity_item_option.id
+        assert option["text"] == activity_item_option.text
+        assert option["image"] == activity_item_option.image
+        assert option["score"] == activity_item_option.score
+        assert option["tooltip"] == activity_item_option.tooltip
+        assert option["isHidden"] == activity_item_option.is_hidden
+        assert option["color"] == activity_item_option.color
+        assert option["alert"] == activity_item_option.alert
+        assert option["value"] == activity_item_option.value
+        config = item["config"]
+        activity_item.config = cast(SingleSelectionConfig, activity_item.config)
+        assert config["removeBackButton"] == activity_item.config.remove_back_button
+        assert config["skippableItem"] == activity_item.config.skippable_item
+        assert config["randomizeOptions"] == activity_item.config.randomize_options
+        assert config["timer"] == activity_item.config.timer
+        assert config["addScores"] == activity_item.config.add_scores
+        assert config["setAlerts"] == activity_item.config.set_alerts
+        assert config["addTooltip"] == activity_item.config.add_tooltip
+        assert config["setPalette"] == activity_item.config.set_palette
+        assert config["addTokens"] == activity_item.config.add_tokens
+        assert (
+            config["additionalResponseOption"]["textInputOption"]
+            == activity_item.config.additional_response_option.text_input_option
+        )
+        assert (
+            config["additionalResponseOption"]["textInputRequired"]
+            == activity_item.config.additional_response_option.text_input_required
+        )
+
+        assert result["details"][0]["scoresAndReports"] == activity.scores_and_reports
+
+        assert activity.id == flow.items[0].activity_id
+        assert result["details"][1]["name"] == flow.name
+        assert result["details"][1]["description"] == flow.description
+        assert result["details"][1]["type"] == "activityFlow"
+
+        items = result["details"][1]["items"]
+        flow_item = flow.items[0]
+        assert len(items) == 1
+        assert len(items) == len(flow.items)
+        item = items[0]
+        assert item["id"] == str(flow_item.id)
+        assert item["activityId"] == str(flow_item.activity_id)
+        assert item["order"] == flow_item.order
 
     async def test_public_activity_detail(self, client, applet_one_with_public_link: AppletFull):
         activity = applet_one_with_public_link.activities[0]
