dace56e7a86bdce87f8770faebcd1c362632b273
ibogretsov <ilyabogretsov@gmail.com>
2024-04-16 13:40:25 +0300

    M2-6048: Add explicit getting env variable and comment

--- src/config/opentelemetry.py
+++ src/config/opentelemetry.py
@@ -1,5 +1,6 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class OpenTelemetrySettings(BaseSettings):
-    otel_exporter_otlp_traces_endpoint: str = ""
+    # We use variables for opentelemetry without prefix OPENTELEMETRY__ to avoid conflicts with opentelemetry.sdk
+    otel_exporter_otlp_traces_endpoint: str | None = Field(None, env="OTEL_EXPORTER_OTLP_TRACES_ENDPOINT")
