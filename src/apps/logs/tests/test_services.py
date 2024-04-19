f049b0b39bf13ffe69aac7b840a0ea4f677083f2
ibogretsov <ilyabogretsov@gmail.com>
2024-04-19 17:34:59 +0300

    M2-4180: Fix failed tests after sync with develop

--- src/apps/logs/tests/test_services.py
+++ src/apps/logs/tests/test_services.py
@@ -7,16 +7,16 @@ from apps.users.db.schemas import UserSchema
 from infrastructure.http.domain import MindloggerContentSource
 
 
-async def test_create_log_success(session: AsyncSession, user_tom: UserSchema, base_log_data: dict[str, Any]) -> None:
-    base_log_data["user_id"] = user_tom.id
+async def test_create_log_success(session: AsyncSession, tom: UserSchema, base_log_data: dict[str, Any]) -> None:
+    base_log_data["user_id"] = tom.id
     log = await UserActivityLogService(session).create_log(**base_log_data)
-    assert log.user_id == user_tom.id
+    assert log.user_id == tom.id
 
 
 async def test_create_log_undefined_with_device_is_mobile_content(
-    session: AsyncSession, user_tom: UserSchema, base_log_data: dict[str, Any]
+    session: AsyncSession, tom: UserSchema, base_log_data: dict[str, Any]
 ) -> None:
-    base_log_data["user_id"] = user_tom.id
+    base_log_data["user_id"] = tom.id
     base_log_data["firebase_token_id"] = "token"
     base_log_data["mindlogger_content"] = MindloggerContentSource.undefined
     log = await UserActivityLogService(session).create_log(**base_log_data)
