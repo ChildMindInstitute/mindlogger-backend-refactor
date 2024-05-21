be32f81958f97f771fbce682be314bdeebe28830
Damirkhon Aloev <aloevdamirkhon@gmail.com>
2024-05-22 00:11:55 +0500

    M2-6726: fix code quality

--- src/apps/activities/domain/custom_validation_subscale.py
+++ src/apps/activities/domain/custom_validation_subscale.py
@@ -5,10 +5,10 @@ def validate_score_subscale_table(value: str):
     # make sure it's format is "x~y" or "x"
     if "~" not in value:
         # make sure x is float with 5 decimal points
-        x: float
+        val: float
         try:
-            x = float(value)  # noqa: F841
-            if len(str(x).split(".")[1]) > 5:
+            val = float(value)  # noqa: F841
+            if len(str(val).split(".")[1]) > 5:
                 raise InvalidScoreSubscaleError()
         except ValueError:
             raise InvalidScoreSubscaleError()
@@ -34,10 +34,10 @@ def validate_raw_score_subscale(value: str):
     # make sure it's format is "x~y" or "x"
     if "~" not in value:
         # make sure x is float with 5 decimal points
-        x: float
+        val: float
         try:
-            x = float(value)  # noqa: F841
-            if len(str(x).split(".")[1]) > 2:
+            val = float(value)  # noqa: F841
+            if len(str(val).split(".")[1]) > 2:
                 raise InvalidRawScoreSubscaleError()
         except ValueError:
             raise InvalidRawScoreSubscaleError()
