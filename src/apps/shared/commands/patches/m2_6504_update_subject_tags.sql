0d160c9acb7b800348b3c973733a5eaff577ff6c
Rodrigo Merlo <rodrigo.merlo@metalab.com>
2024-06-04 14:16:34 -0300

    Fix: Multiinformant patch to considerate applet context in query

--- src/apps/shared/commands/patches/m2_6504_update_subject_tags.sql
+++ src/apps/shared/commands/patches/m2_6504_update_subject_tags.sql
@@ -4,6 +4,6 @@ set
 	tag = 'Team'
 from
 	user_applet_accesses uaa
-where
-	sj.user_id = uaa.user_id
-	and uaa.role != 'respondent';
+where sj.user_id = uaa.user_id
+  and sj.applet_id = uaa.applet_id
+  and uaa.role != 'respondent';
