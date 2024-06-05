53a7fb60d7b50e7d20fb17aab63bb4b3a338f7ab
Andrew Weiland <aweiland@users.noreply.github.com>
2024-06-05 10:50:00 -0400

    fix: Fixed overwriting copilot manifest on initial creation

--- copilot/scripts/env-deploy.sh
+++ copilot/scripts/env-deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# This is done in case the deploy failed but the env deploy succeeded on a previous run
-# The manifest may not exist
-mkdir -p copilot/environments/$ENV_NAME
-copilot env show -n $ENV_NAME --manifest > copilot/environments/$ENV_NAME/manifest.yml
+
+if [ ! -f copilot/environments/$ENV_NAME/manifest.yml ]; then
+  # This is done in case the deploy failed but the env init succeeded on a previous run
+  # The manifest may not exist
+  mkdir -p copilot/environments/$ENV_NAME
+  copilot env show -n $ENV_NAME --manifest > copilot/environments/$ENV_NAME/manifest.yml
+fi
 
 #Copilot checks if there are changes automatically
 copilot env deploy \
