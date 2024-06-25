807bb64e5d2bc5f8b24e08e67c87497b00867882
Eugene Us <eus@scnsoft.com>
2024-06-25 15:15:48 +0300

    fix: added ability to not provide consent_to_share for applet answer

--- src/apps/answers/domain/answers.py
+++ src/apps/answers/domain/answers.py
@@ -100,7 +100,7 @@ class AppletAnswerCreate(InternalModel):
     client: ClientMeta
     target_subject_id: uuid.UUID | None
     source_subject_id: uuid.UUID | None
-    consent_to_share: bool
+    consent_to_share: bool | None
 
     _dates_from_ms = validator("created_at", pre=True, allow_reuse=True)(datetime_from_ms)
 
