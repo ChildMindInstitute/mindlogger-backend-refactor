55c08c79d8a0fe64248a7794e6f10e1d2a6af688
Kenroy Gobourne <kenroy.gobourne@gmail.com>
2024-07-23 11:53:39 -0500

    fix: Failing unit tests (M2-7414) (#1505)
    This PR fixes the broken unit tests from #1494, and adds some tests that were missing from that PR.

--- src/apps/subjects/api.py
+++ src/apps/subjects/api.py
@@ -232,7 +232,9 @@ async def get_subject(
         raise NotFoundError()
 
     user_subject = await subjects_service.get_by_user_and_applet(user.id, subject.applet_id)
-    if user_subject:
+    if not user_subject:
+        await CheckAccessService(session, user.id).check_subject_subject_access(subject.applet_id, subject_id)
+    else:
         relation = await subjects_service.get_relation(user_subject.id, subject_id)
         has_relation = relation is not None and (
             relation.relation != "take-now" or is_valid_take_now_relation(relation)
