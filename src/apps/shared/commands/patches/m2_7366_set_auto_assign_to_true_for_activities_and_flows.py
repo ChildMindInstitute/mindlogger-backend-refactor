fb2f951ebea9f5c3b6728fe85bb11db2a5532864
Coro <alejandro.coronado.n@gmail.com>
2024-08-09 12:50:26 -0600

    Formated new patch with ruff, deleted setting auto_assign default to true value, since it isn't part of the AC's

--- src/apps/shared/commands/patches/m2_7366_set_auto_assign_to_true_for_activities_and_flows.py
+++ src/apps/shared/commands/patches/m2_7366_set_auto_assign_to_true_for_activities_and_flows.py
@@ -2,15 +2,7 @@
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-# SQL Queries
-SET_DEFAULT_AUTO_ASSIGN_ACTIVITIES_SQL = """
-    ALTER TABLE activities ALTER COLUMN auto_assign SET DEFAULT true;
-"""
-
-SET_DEFAULT_AUTO_ASSIGN_FLOWS_SQL = """
-    ALTER TABLE flows ALTER COLUMN auto_assign SET DEFAULT true;
-"""
-
+# SQL Queries to update existing records
 UPDATE_EXISTING_ACTIVITIES_SQL = """
     UPDATE activities SET auto_assign = true WHERE auto_assign IS NULL;
 """
@@ -19,12 +11,9 @@ UPDATE_EXISTING_FLOWS_SQL = """
     UPDATE flows SET auto_assign = true WHERE auto_assign IS NULL;
 """
 
+
 async def main(session: AsyncSession, *args, **kwargs):
     try:
-        # Set the default value of auto_assign to true for new records
-        await session.execute(SET_DEFAULT_AUTO_ASSIGN_ACTIVITIES_SQL)
-        await session.execute(SET_DEFAULT_AUTO_ASSIGN_FLOWS_SQL)
-
         # Update existing records to set auto_assign to true
         await session.execute(UPDATE_EXISTING_ACTIVITIES_SQL)
         await session.execute(UPDATE_EXISTING_FLOWS_SQL)
