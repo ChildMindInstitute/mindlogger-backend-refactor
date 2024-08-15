f638dae4f3dc13b5829962c65db2c1647b963618
Rodrigo Colao Merlo <rodrigo.merlo@metalab.com>
2024-08-15 16:24:35 -0300

    feature: Add new get endpoint to return all assignment for an applet (M2-7382) (#1533)

--- src/apps/activity_assignments/api.py
+++ src/apps/activity_assignments/api.py
@@ -2,11 +2,16 @@ import uuid
 
 from fastapi import Body, Depends
 
-from apps.activity_assignments.domain.assignments import ActivitiesAssignments, ActivitiesAssignmentsCreate
+from apps.activity_assignments.domain.assignments import (
+    ActivitiesAssignments,
+    ActivitiesAssignmentsCreate,
+    ActivityAssignmentsListQueryParams,
+)
 from apps.activity_assignments.service import ActivityAssignmentService
 from apps.applets.service import AppletService
 from apps.authentication.deps import get_current_user
 from apps.shared.domain import Response
+from apps.shared.query_params import QueryParams, parse_query_params
 from apps.users import User
 from apps.workspaces.service.check_access import CheckAccessService
 from infrastructure.database import atomic
@@ -24,6 +29,23 @@ async def assignments_create(
         await service.exist_by_id(applet_id)
         await CheckAccessService(session, user.id).check_applet_activity_assignment_access(applet_id)
         assignments = await ActivityAssignmentService(session).create_many(applet_id, schema.assignments)
+
+    return Response(
+        result=ActivitiesAssignments(
+            applet_id=applet_id,
+            assignments=assignments,
+        )
+    )
+
+
+async def applet_assignments(
+    applet_id: uuid.UUID,
+    user: User = Depends(get_current_user),
+    query_params: QueryParams = Depends(parse_query_params(ActivityAssignmentsListQueryParams)),
+    session=Depends(get_session),
+) -> Response[ActivitiesAssignments]:
+    await CheckAccessService(session, user.id).check_applet_activity_assignment_access(applet_id)
+    assignments = await ActivityAssignmentService(session).get_all(applet_id, query_params)
 
     return Response(
         result=ActivitiesAssignments(
