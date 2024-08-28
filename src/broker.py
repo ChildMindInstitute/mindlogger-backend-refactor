6c840899b40ad1576a4c9bd768f89eb34860916b
Rodrigo Merlo <rodrigo.merlo@metalab.com>
2024-08-28 10:10:57 -0300

    chore: Update some dependencies to fix some security issues

--- src/broker.py
+++ src/broker.py
@@ -1,13 +1,18 @@
 import taskiq_fastapi
-from taskiq import InMemoryBroker
+from taskiq import AsyncBroker, InMemoryBroker
+from taskiq.formatters.json_formatter import JSONFormatter
 from taskiq_aio_pika import AioPikaBroker
 from taskiq_redis import RedisAsyncResultBackend
 
 from config import settings
 
-broker = AioPikaBroker(settings.rabbitmq.url).with_result_backend(RedisAsyncResultBackend(settings.redis.url))
+broker: AsyncBroker = (
+    AioPikaBroker(settings.rabbitmq.url)
+    .with_result_backend(RedisAsyncResultBackend(settings.redis.url))
+    .with_formatter(JSONFormatter())
+)
 
 if settings.env == "testing":
-    broker = InMemoryBroker()
+    broker = InMemoryBroker().with_formatter(JSONFormatter())
 
 taskiq_fastapi.init(broker, "main:app")
