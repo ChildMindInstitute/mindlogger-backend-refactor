7415df122a4178bbfd695aaed6416ec0ddb84722
vshvechko <vladimir.shvechko@gmail.com>
2024-08-28 16:54:11 +0300

    feat: Create cli command to transfer applet ownership (M2-7719)

--- src/cli.py
+++ src/cli.py
@@ -9,6 +9,7 @@ import typer  # noqa: E402
 
 from apps.activities.commands import activities  # noqa: E402
 from apps.answers.commands import convert_assessments  # noqa: E402
+from apps.applets.commands import applet_cli  # noqa: E402
 from apps.applets.commands import applet_ema_cli  # noqa: E402
 from apps.shared.commands import encryption_cli, patch  # noqa: E402
 from apps.users.commands import token_cli  # noqa: E402
@@ -22,6 +23,7 @@ cli.add_typer(token_cli, name="token")
 cli.add_typer(patch, name="patch")
 cli.add_typer(encryption_cli, name="encryption")
 cli.add_typer(applet_ema_cli, name="applet-ema")
+cli.add_typer(applet_cli, name="applet")
 
 if __name__ == "__main__":
     # with app context?
