763819adce29f6ee7b13c1d2d0e3401d1eb5705c
David Alberto Montaño Fetecua <david.montano@metalab.com>
2024-09-17 18:50:03 -0500

    fix: removing enable and disable integration endpoints

--- src/apps/integrations/router.py
+++ src/apps/integrations/router.py
@@ -1,35 +1,13 @@
 from fastapi.routing import APIRouter
 from starlette import status
 
-from apps.integrations.api import create_integration, disable_integration, enable_integration, retrieve_integration
+from apps.integrations.api import create_integration, retrieve_integration
 from apps.integrations.domain import Integration
 from apps.shared.domain import Response
 from apps.shared.domain.response import AUTHENTICATION_ERROR_RESPONSES, DEFAULT_OPENAPI_RESPONSE
 
 router = APIRouter(prefix="/integrations", tags=["Integration"])
 
-
-router.post(
-    "/enable_integration",
-    description="This endpoint is used to enable integration\
-           options for a workspace",
-    status_code=status.HTTP_200_OK,
-    responses={
-        **DEFAULT_OPENAPI_RESPONSE,
-        **AUTHENTICATION_ERROR_RESPONSES,
-    },
-)(enable_integration)
-
-router.delete(
-    "/disable_integration",
-    description="This endpoint is used to remove integrations\
-           from a workspace",
-    status_code=status.HTTP_204_NO_CONTENT,
-    responses={
-        **DEFAULT_OPENAPI_RESPONSE,
-        **AUTHENTICATION_ERROR_RESPONSES,
-    },
-)(disable_integration)
 
 router.post(
     path="/",
