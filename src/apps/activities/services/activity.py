f76fc3e673b469c0f2b3adca9efe042f2a4badd0
sultanofcardio <kenroy.gobourne@gmail.com>
2024-09-30 09:40:43 -0500

    Remove early return statement
    This was causing the endpoint to return an empty list in the case where there are only auto assigned activities/flows

--- src/apps/activities/services/activity.py
+++ src/apps/activities/services/activity.py
@@ -458,9 +458,6 @@ class ActivityService:
     async def get_activity_and_flow_basic_info_by_ids_or_auto(
         self, applet_id: uuid.UUID, ids: list[uuid.UUID], language: str
     ) -> list[ActivityOrFlowBasicInfoInternal]:
-        if len(ids) == 0:
-            return []
-
         return await ActivitiesCRUD(self.session).get_activity_and_flow_basic_info_by_ids_or_auto(
             applet_id, ids, language
         )
