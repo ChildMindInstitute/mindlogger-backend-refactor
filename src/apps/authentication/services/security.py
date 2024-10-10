f4626755704adf63fc74be23e8ce5abd76c6268c
Rodrigo Merlo <rodrigo.merlo@metalab.com>
2024-10-10 13:01:37 -0300

    chore: Moving from python-jose  lib to pyJWT

--- src/apps/authentication/services/security.py
+++ src/apps/authentication/services/security.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime, timedelta, timezone
 
-from jose import jwt
+import jwt
 
 from apps.authentication.domain.login import UserLoginRequest
 from apps.authentication.domain.token import InternalToken, JWTClaim, TokenPayload, TokenPurpose
