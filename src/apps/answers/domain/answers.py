04db271af8671f6e9807ad507494214499f5cfb8
André Vital <andre@andrevital.com>
2024-11-29 00:00:06 -0600

    feature: Extend UserAnswerDataBase with additional props for subjects

--- src/apps/answers/domain/answers.py
+++ src/apps/answers/domain/answers.py
@@ -526,10 +526,16 @@ class UserAnswerDataBase(BaseModel):
     submit_id: uuid.UUID
     version: str
     respondent_id: uuid.UUID | str | None = None
-    target_subject_id: uuid.UUID | str | None = None
-    target_secret_id: uuid.UUID | str | None = None
     source_subject_id: uuid.UUID | str | None = None
     source_secret_id: uuid.UUID | str | None = None
+    source_user_nickname: str | None = None
+    target_subject_id: uuid.UUID | str | None = None
+    target_secret_id: uuid.UUID | str | None = None
+    target_user_nickname: str | None = None
+    target_user_tag: str | None = None
+    input_subject_id: uuid.UUID | str | None = None
+    input_secret_id: uuid.UUID | str | None = None
+    input_user_nickname: str | None = None
     relation: str | None = None
     respondent_secret_id: str | None = None
     legacy_profile_id: str | None = None
