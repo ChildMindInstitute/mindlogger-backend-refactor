7abe7811b1e5ce6665d92a51ae3b9c46e0ee10e9
André Vital <andre@andrevital.com>
2024-12-05 06:00:36 -0600

    feat: Report answer export updates (M2-7255) (#1670)
    * feature: Add nickname and tag to subject export data
    
    * feature: Add input_subject_id to answer data fetching
    
    * feature: Extend UserAnswerDataBase with additional props for subjects
    
    * feature: Add mapping for input subject in answer data export
    
    * feature: Remap respondent secret id to logged in user
    
    * feature: Add source user tag prop to export
    
    * feature: Remap respondent assignment to logged in user
    
    * fix: Remove unneeded query
    
    * fix: Add missing type: ignore comment
    
    * fix: Add missing expected keys fields for test
    
    * fix: Add missing expected keys fields for test (arbitrary)
    
    * fix: File formatting
    
    * fix: Correct respondent secret id assignment
    
    * fix: Add type ignore comments
    
    * fix: Update expected secret id for test
    
    * fix: Update expected secret id to respondent for test
    
    * fix: Update validation for secret id

--- src/apps/answers/tests/test_answers.py
+++ src/apps/answers/tests/test_answers.py
@@ -1840,9 +1840,11 @@ class TestAnswerActivityItems(BaseTest):
             "flowName", "id", "itemIds", "migratedData", "respondentId",
             "respondentSecretId", "reviewedAnswerId", "userPublicKey",
             "version", "submitId", "scheduledDatetime", "startDatetime",
-            "endDatetime", "legacyProfileId", "migratedDate",
-            "relation", "sourceSubjectId", "sourceSecretId", "targetSubjectId",
-            "targetSecretId", "client", "tzOffset", "scheduledEventId", "reviewedFlowSubmitId"
+            "endDatetime", "legacyProfileId", "migratedDate", "relation",
+            "sourceSubjectId", "sourceSecretId", "sourceUserNickname", "sourceUserTag",
+            "targetSubjectId", "targetSecretId", "targetUserNickname", "targetUserTag",
+            "inputSubjectId", "inputSecretId", "inputUserNickname",
+            "client", "tzOffset", "scheduledEventId", "reviewedFlowSubmitId"
         }
         # Comment for now, wtf is it
         # assert int(answer['startDatetime'] * 1000) == answer_item_create.start_time
@@ -1874,7 +1876,8 @@ class TestAnswerActivityItems(BaseTest):
         assert len(data["answers"]) == 1
         assert resp_data["count"] == 1
         assert data["answers"][0]["respondentId"] == str(tom.id)
-        assert data["answers"][0]["respondentSecretId"] == answer_shell_account_target["target_secret_user_id"]
+        respondent_secret_id = data["answers"][0]["respondentSecretId"].split(" ")[-1].strip("()")
+        assert respondent_secret_id == answer_shell_account_target["respondent_secret_user_id"]
 
     @pytest.mark.parametrize(
         "user_fixture, exp_cnt",
