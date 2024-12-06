c8552605c03e4eb1ac457c144f7c546685d4afde
Andrew Weiland <aweiland@users.noreply.github.com>
2024-12-06 13:23:05 -0500

    Works?

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 import os
+import logging
 
 # This line needs to be run before any `ddtrace` import, to avoid sending traces
 # in local dev environment (we don't have a Datadog agent configured locally, so
 # it prints a stacktrace every time it tries to send a trace)
-os.environ["DD_TRACE_ENABLED"] = os.getenv("DD_TRACE_ENABLED", "false")  # noqa
+# os.environ["DD_TRACE_ENABLED"] = os.getenv("DD_TRACE_ENABLED", "false")  # noqa
 
 # Import DataDog tracer ASAP
 if os.getenv("DD_TRACE_ENABLED", "false").lower() == "true":
-    import ddtrace.auto  # noqa
+    logging.getLogger("main").setLevel(logging.INFO)
+    logging.getLogger("main").addHandler(logging.StreamHandler())
+    logging.getLogger("main").info("Enabling Datadog")
+    # import ddtrace.auto  # noqa
+#     import ddtrace
+#     ddtrace.patch_all()
 
 from infrastructure.app import create_app
 
