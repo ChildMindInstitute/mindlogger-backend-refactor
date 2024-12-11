9a74e6e7881cb3779dd980286da1aeaa556814d0
Andrew Weiland <aweiland@users.noreply.github.com>
2024-12-11 08:59:59 -0500

    chore: Add structured logging for DataDog integration (M2-8041) (#1675)
    * Adding DD dependencies
    
    * Better json logging
    
    * More logging
    
    * Structured logging
    
    * More logging
    
    * docker tweak
    
    * Fixes
    
    * fixed test suite
    
    * Minor refactor
    
    * Tweaks
    
    * Datadog in feature env
    
    * Fixed startup
    
    * sidecars
    
    * Fixing manifest
    
    * Works?
    
    * Datadog tweaks
    
    * Patching works
    
    * test fixes
    
    * Manifest update
    
    * fixed build/deploy

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
+import os
+import logging
+
+# Import DataDog tracer ASAP
+if os.getenv("DD_TRACE_ENABLED", "false").lower() == "true":
+    logging.getLogger("main").setLevel(logging.INFO)
+    logging.getLogger("main").addHandler(logging.StreamHandler())
+    logging.getLogger("main").info("Enabling Datadog")
+    # import ddtrace.auto  # noqa
+    from ddtrace import patch
+    # Manually patch.  The auto patcher throws some errors in AMQP (which it doesn't support so why patch it??)
+    patch(sqlalchemy=True, fastapi=True, botocore=True, asyncpg=True, httpx=True, jinja2=True,
+          requests=True, starlette=True, structlog=True)
+
+
 from infrastructure.app import create_app
 
 app = create_app()
-
 
 # @app.on_event("startup")
 # async def create_superuser():
