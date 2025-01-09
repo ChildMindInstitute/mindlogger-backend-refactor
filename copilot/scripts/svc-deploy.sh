9709278aaffb4905decab031d9f916817f00303d
Andrew Weiland <aweiland@users.noreply.github.com>
2025-01-09 11:41:11 -0500

    platform env var

--- copilot/scripts/svc-deploy.sh
+++ copilot/scripts/svc-deploy.sh
@@ -3,7 +3,7 @@
 
 echo "Deploying service $COPILOT_SERVICE to $ENV_NAME"
 
-copilot svc deploy \
+DOCKER_DEFAULT_PLATFORM=linux/arm64 copilot svc deploy \
     --name "$COPILOT_SERVICE" \
     --env "$ENV_NAME" \
     --force \
