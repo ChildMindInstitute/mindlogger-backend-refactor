3f7b438de72969394f013097143866173038535f
Hamza <hamza.nanouche@ifea.education>
2025-01-31 16:14:19 +0100

    fixup! feat: save prolific participant information on submission

---
+++ src/infrastructure/database/migrations/versions/2025_01_31_16_13-.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: e70c5448c7d2
+Revises: 032d8458aa63, 1e448d205193
+Create Date: 2025-01-31 16:13:36.304868
+
+"""
+
+
+
+# revision identifiers, used by Alembic.
+revision = "e70c5448c7d2"
+down_revision = ("032d8458aa63", "1e448d205193")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
