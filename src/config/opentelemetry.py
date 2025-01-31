aed9e8bc7df1c06463ce3090d436836eabe8fd12
Andrew Weiland <aweiland@users.noreply.github.com>
2025-01-31 10:41:37 -0500

    chore: Remove Open Telemetry (M2-8419) (#1720)
    * Removing open telemetry
    
    * Removed compose/opentelemetry

--- src/config/opentelemetry.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseSettings, Field
-
-
-class OpenTelemetrySettings(BaseSettings):
-    # We use variables for opentelemetry without prefix OPENTELEMETRY__ to avoid conflicts with opentelemetry.sdk
-    otel_exporter_otlp_traces_endpoint: str | None = Field(None, env="OTEL_EXPORTER_OTLP_TRACES_ENDPOINT")
