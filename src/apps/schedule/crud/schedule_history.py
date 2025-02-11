566f05000f9e015012263d1fdc33709f7cfb2d6f
Kenroy Gobourne <14842108+sultanofcardio@users.noreply.github.com>
2025-02-11 11:33:25 -0500

    feat: Implement schedule change versioning + data migration (M2-8494) (#1719)
    This PR creates several data migrations to support the new and modified tables
    
    #### `events` table
    
    - The data from the periodicity table is migrated to their equivalent columns in the events table.
    - A version number is generated for all existing rows, based on the date in the `updated_at` column
    - The version column has a default value based on the current date, where `n` is set to 1
    - The periodicity and version columns are made non-nullable since they are now being populated
    - The foreign key constraint has been removed from the `periodicity_id` column. This is so that new records can be added to the table with this value being a random UUID (used in the down migration in case the periodicity data needs to be moved back to that table)
    
    #### Event history tables
    
    - An initial entry is made to `event_histories` for each row in `events`, since this will be done for every new entry
    - `applet_events` is populated based on each version of the applet that references a version of each schedule
    - `notification_histories` and `reminder_histories` are populated based on the data in `notifications` and `reminders` respectively
    
    #### Schedule history tracking
    
    In addition to these data migrations, the API code has been updated in several places to keep track of schedule history. These changes come down to the following:
    - The creation of a new schedule triggers equivalent entries into the history tables
    - Any update to the schedule triggers an update to the version number in the `events` table, and new entries into the history tables
    - Any update to an applet that changes its version number triggers new entries into the `applet_events` table, pointing to the same version of the schedule
    - Deletion of a schedule marks it as deleted in the history tables

---
+++ src/apps/schedule/crud/schedule_history.py
@@ -0,0 +1,80 @@
+__all__ = ["ScheduleHistoryCRUD", "AppletEventsCRUD", "NotificationHistoryCRUD", "ReminderHistoryCRUD"]
+
+import asyncio
+import uuid
+
+from sqlalchemy import update
+
+from apps.schedule.db.schemas import (
+    AppletEventsSchema,
+    EventHistorySchema,
+    NotificationHistorySchema,
+    ReminderHistorySchema,
+)
+from infrastructure.database import BaseCRUD
+
+
+class ScheduleHistoryCRUD(BaseCRUD[EventHistorySchema]):
+    async def add(self, event: EventHistorySchema) -> EventHistorySchema:
+        return await self._create(event)
+
+    async def mark_as_deleted(self, events: list[tuple[uuid.UUID, str]]):
+        id_versions = [f"{event[0]}_{event[1]}" for event in events]
+
+        query = update(EventHistorySchema).where(EventHistorySchema.id_version.in_(id_versions)).values(is_deleted=True)
+
+        await self._execute(query)
+
+        await asyncio.gather(
+            AppletEventsCRUD(self.session).mark_as_deleted(events),
+            NotificationHistoryCRUD(self.session).mark_as_deleted(events),
+            ReminderHistoryCRUD(self.session).mark_as_deleted(events),
+        )
+
+
+class AppletEventsCRUD(BaseCRUD[AppletEventsSchema]):
+    async def add(self, applet_event: AppletEventsSchema) -> AppletEventsSchema:
+        return await self._create(applet_event)
+
+    async def add_many(self, applet_events: list[AppletEventsSchema]) -> list[AppletEventsSchema]:
+        return await self._create_many(applet_events)
+
+    async def mark_as_deleted(self, events: list[tuple[uuid.UUID, str]]):
+        id_versions = [f"{event[0]}_{event[1]}" for event in events]
+
+        query = update(AppletEventsSchema).where(AppletEventsSchema.event_id.in_(id_versions)).values(is_deleted=True)
+
+        await self._execute(query)
+
+
+class NotificationHistoryCRUD(BaseCRUD[NotificationHistorySchema]):
+    async def add(self, notification: NotificationHistorySchema) -> NotificationHistorySchema:
+        return await self._create(notification)
+
+    async def add_many(self, notifications: list[NotificationHistorySchema]) -> list[NotificationHistorySchema]:
+        return await self._create_many(notifications)
+
+    async def mark_as_deleted(self, events: list[tuple[uuid.UUID, str]]):
+        id_versions = [f"{event[0]}_{event[1]}" for event in events]
+
+        query = (
+            update(NotificationHistorySchema)
+            .where(NotificationHistorySchema.event_id.in_(id_versions))
+            .values(is_deleted=True)
+        )
+
+        await self._execute(query)
+
+
+class ReminderHistoryCRUD(BaseCRUD[ReminderHistorySchema]):
+    async def add(self, reminder: ReminderHistorySchema) -> ReminderHistorySchema:
+        return await self._create(reminder)
+
+    async def mark_as_deleted(self, events: list[tuple[uuid.UUID, str]]):
+        id_versions = [f"{event[0]}_{event[1]}" for event in events]
+
+        query = (
+            update(ReminderHistorySchema).where(ReminderHistorySchema.event_id.in_(id_versions)).values(is_deleted=True)
+        )
+
+        await self._execute(query)
