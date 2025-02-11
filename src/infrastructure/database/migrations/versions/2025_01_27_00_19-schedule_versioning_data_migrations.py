9f0f19905d021f2f6b8876e81d68fd108a22139e
Kenroy Gobourne <14842108+sultanofcardio@users.noreply.github.com>
2025-02-11 11:33:25 -0500

    feat: Implement schedule change versioning + data migration (M2-8494) (#1719)
    This PR creates several data migrations to support the new and modified tables
    
    #### `events` table
    
    - The data from the periodicity table is migrated to their equivalent columns in the events table.
    - A version number is generated for all existing rows, based on the date in the `updated_at` column
    - The version column has a default value based on the current date, where `n` is set to 1
    - The periodicity and version columns are made non-nullable since they are now being populated
    - The foreign key constraint has been removed from the `periodicity_id` column. This is so that new records can be added to the table with this value being a random UUID (used in the down migration in case the periodicity data needs to be moved back to that table)
    
    #### Event history tables
    
    - An initial entry is made to `event_histories` for each row in `events`, since this will be done for every new entry
    - `applet_events` is populated based on each version of the applet that references a version of each schedule
    - `notification_histories` and `reminder_histories` are populated based on the data in `notifications` and `reminders` respectively
    
    #### Schedule history tracking
    
    In addition to these data migrations, the API code has been updated in several places to keep track of schedule history. These changes come down to the following:
    - The creation of a new schedule triggers equivalent entries into the history tables
    - Any update to the schedule triggers an update to the version number in the `events` table, and new entries into the history tables
    - Any update to an applet that changes its version number triggers new entries into the `applet_events` table, pointing to the same version of the schedule
    - Deletion of a schedule marks it as deleted in the history tables

---
+++ src/infrastructure/database/migrations/versions/2025_01_27_00_19-schedule_versioning_data_migrations.py
@@ -0,0 +1,198 @@
+"""Schedule versioning data migrations
+
+Revision ID: 7c7e30fa96a4
+Revises: 62b491c18ace
+Create Date: 2025-01-27 00:19:06.039296
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = "7c7e30fa96a4"
+down_revision = "62b491c18ace"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Populate `events` from `periodicity`
+    # We won't delete the periodicity table yet, but it's no longer being used
+    op.execute("""
+    UPDATE events
+    SET periodicity = periodicity.type,
+    start_date = periodicity.start_date,
+    end_date = periodicity.end_date,
+    selected_date = periodicity.selected_date
+    FROM periodicity
+    WHERE events.periodicity_id = periodicity.id
+    AND events.periodicity IS NULL
+    """)
+
+    # Account for any leftover null `periodicity` values in `events` by setting to `NONE`
+    # Based on the current constraints, I don't expect there to be any but just to be safe
+    op.execute("UPDATE events SET periodicity = 'NONE' WHERE periodicity IS NULL")
+
+    # Remove foreign key constraint on `periodicity_id` and set a default random UUID for new entries
+    op.drop_constraint("fk_events_periodicity_id_periodicity", "events", type_="foreignkey")
+    op.alter_column("events", "periodicity_id", server_default=sa.text("gen_random_uuid()"))
+
+    # Generate version number for entries in `events`. We use `updated_at` instead of `created_at` because
+    # the value in the `version` field is supposed to represent the latest version of the event
+    op.execute("""
+    UPDATE events
+    SET version = TO_CHAR(updated_at, 'YYYYMMDD') || '-1'
+    WHERE version IS NULL
+    """)
+
+    # Make `events.periodicity` non-nullable
+    op.alter_column("events", "periodicity", existing_type=sa.String(length=10), nullable=False)
+
+    # Make `events.version` non-nullable and add a default value
+    op.alter_column(
+        "events",
+        "version",
+        existing_type=sa.String(length=13),
+        server_default=sa.text("TO_CHAR(timezone('utc', now()), 'YYYYMMDD') || '-1'"),
+        nullable=False
+    )
+
+    # Populate `event_histories` from `events`, `activity_events`, `flow_events`, and `user_events`
+    op.execute("""
+    INSERT INTO event_histories (is_deleted, created_at, updated_at, migrated_date, migrated_updated, start_time, end_time,
+                             access_before_schedule, one_time_completion, timer, timer_type, version, periodicity,
+                             start_date, end_date, selected_date, id_version, id, event_type, activity_id,
+                             activity_flow_id, user_id)
+    SELECT e.is_deleted,
+       e.created_at,
+       e.updated_at,
+       e.migrated_date,
+       e.migrated_updated,
+       e.start_time,
+       e.end_time,
+       e.access_before_schedule,
+       e.one_time_completion,
+       e.timer,
+       e.timer_type,
+       e.version,
+       e.periodicity,
+       e.start_date,
+       e.end_date,
+       e.selected_date,
+       e.id || '_' || e.version as id_version,
+       e.id,
+       (CASE WHEN ae.activity_id IS NOT NULL THEN 'activity' ELSE 'flow' END)::event_type_enum AS event_type,
+       ae.activity_id,
+       fe.flow_id as activity_flow_id,
+       ue.user_id
+    FROM events e
+    LEFT JOIN activity_events ae ON e.id = ae.event_id
+    LEFT JOIN flow_events fe ON e.id = fe.event_id
+    LEFT JOIN user_events ue ON e.id = ue.event_id;
+    """)
+
+    # Populate `applet_events`
+    op.execute("""
+    INSERT INTO applet_events (is_deleted, applet_id, event_id)
+    SELECT FALSE AS is_deleted,
+       e.applet_id || '_' || a.version as applet_id,
+       eh.id_version AS event_id
+    FROM events e
+    JOIN event_histories eh ON eh.id_version = (e.id || '_' || e.version)
+    JOIN applets a ON a.id = e.applet_id;
+    """)
+
+    # Populate `notification_histories`
+    op.execute("""
+    INSERT INTO notification_histories (is_deleted, created_at, updated_at, migrated_date, migrated_updated, from_time,
+                                    to_time, at_time, trigger_type, "order", id_version, id, event_id) 
+    SELECT n.is_deleted,
+         n.created_at,
+            n.updated_at,
+            n.migrated_date,
+            n.migrated_updated,
+            n.from_time,
+            n.to_time,
+            n.at_time,
+            n.trigger_type,
+            n."order",
+            n.id || '_' || e.version as id_version,
+            n.id,
+            e.id || '_' || e.version as event_id
+    FROM notifications n
+    JOIN events e ON e.id = n.event_id;
+    """)
+
+    # Populate `reminder_histories`
+    op.execute("""
+    INSERT INTO reminder_histories(is_deleted, created_at, updated_at, migrated_date, migrated_updated, activity_incomplete,
+                               reminder_time, id_version, id, event_id)    
+    SELECT r.is_deleted,
+       r.created_at,
+       r.updated_at,
+       r.migrated_date,
+       r.migrated_updated,
+       r.activity_incomplete,
+       r.reminder_time,
+       r.id || '_' || e.version as id_version,
+       r.id,
+       e.id || '_' || e.version as event_id
+    FROM reminders r
+    JOIN events e ON e.id = r.event_id;
+    """)
+
+
+def downgrade() -> None:
+    # Empty the `applet_events` table
+    op.execute("DELETE FROM applet_events WHERE TRUE")
+
+    # Empty the `notification_histories` table
+    op.execute("DELETE FROM notification_histories WHERE TRUE")
+
+    # Empty the `reminder_histories` table
+    op.execute("DELETE FROM reminder_histories WHERE TRUE")
+
+    # Empty the `event_histories` table
+    op.execute("DELETE FROM event_histories WHERE TRUE")
+
+    # Make `events.version` nullable
+    op.alter_column("events", "version", existing_type=sa.String(length=10), nullable=True)
+
+    # Make `events.periodicity` nullable
+    op.alter_column("events", "periodicity", existing_type=sa.String(length=10), nullable=True)
+
+    # Remove the generated version numbers from `events`
+    op.execute("UPDATE events SET version = NULL WHERE version IS NOT NULL")
+
+    # Remove the migrated `periodicity` values from `events`
+    op.execute("""
+    UPDATE events
+    SET periodicity = NULL,
+    start_date = NULL,
+    end_date = NULL,
+    selected_date = NULL
+    WHERE periodicity_id IN (SELECT id FROM periodicity)
+    """)
+    op.execute("UPDATE events SET periodicity = NULL WHERE periodicity = 'NONE'")
+
+    # Add missing entries to the `periodicity` table to prep for the foreign key constraint
+    op.execute("""
+    INSERT INTO periodicity(id, type, start_date, end_date, selected_date, is_deleted)
+    SELECT periodicity_id, periodicity, start_date, end_date, selected_date, false
+    FROM events
+    WHERE periodicity_id NOT IN (SELECT id FROM periodicity); 
+    """)
+
+    # Remove the default value for `periodicity_id`
+    op.alter_column("events", "periodicity_id", server_default=None)
+
+    # Add back the foreign key constraint on `periodicity_id`
+    op.create_foreign_key(
+        "fk_events_periodicity_id_periodicity",
+        "events",
+        "periodicity",
+        ["periodicity_id"],
+        ["id"]
+    )
