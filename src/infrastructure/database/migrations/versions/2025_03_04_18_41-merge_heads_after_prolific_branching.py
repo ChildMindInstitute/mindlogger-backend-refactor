a35c9b506110807c69654a720ca6131c28f70115
Carlos Chacon <carlos.chacon@metalab.com>
2025-03-04 18:42:52 -0600

    fix: merge heads after prolific branch

---
+++ src/infrastructure/database/migrations/versions/2025_03_04_18_41-merge_heads_after_prolific_branching.py
@@ -0,0 +1,23 @@
+"""Merge heads after prolific branching
+
+Revision ID: 067a2c34ff2f
+Revises: 11b5cd083450, 8fa31d7d9831
+Create Date: 2025-03-04 18:41:23.038921
+
+"""
+
+
+
+# revision identifiers, used by Alembic.
+revision = "067a2c34ff2f"
+down_revision = ("11b5cd083450", "8fa31d7d9831")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
