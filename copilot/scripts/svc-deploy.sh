90a63ef7c1573caa6e9aeb48241afacdb25e8cc5
Andrew Weiland <aweiland@users.noreply.github.com>
2025-03-31 09:18:59 -0400

    Limit cluster to just this PR

--- copilot/scripts/svc-deploy.sh
+++ copilot/scripts/svc-deploy.sh
@@ -9,46 +9,47 @@ copilot svc deploy \
     --force \
     --diff-yes
 
+PR_NUM=${ENV_NAME}
+
 # Add Datadog tags to ALB
-for PR_NUM in $(aws ecs list-clusters --output text | grep -E -o 'pr\-[0-9]+' | grep -E -o '[0-9]+'); do
-  echo "Working with ${PR_NUM}"
-  ALL_ALBS=$(aws elbv2 describe-load-balancers --query "LoadBalancers[*].LoadBalancerArn" --output text)
-  ALB_ARNS=$(IFS=, ; echo "${ALL_ALBS[*]}")
-  ARN=$(aws elbv2 describe-tags --resource-arns ${ALB_ARNS} --query "TagDescriptions[?Tags[?Key=='copilot-environment' && Value=='pr-${PR_NUM}']].ResourceArn" --output text)
-  echo "ALB ARN: ${ARN}"
+echo "Working with ${PR_NUM}"
+ALL_ALBS=$(aws elbv2 describe-load-balancers --query "LoadBalancers[*].LoadBalancerArn" --output text)
+ALB_ARNS=$(IFS=, ; echo "${ALL_ALBS[*]}")
+ARN=$(aws elbv2 describe-tags --resource-arns "${ALB_ARNS}" --query "TagDescriptions[?Tags[?Key=='copilot-environment' && Value=='pr-${PR_NUM}']].ResourceArn" --output text)
+echo "ALB ARN: ${ARN}"
 
-  DD_VERSION="pr-${PR_NUM}"
-  aws elbv2 add-tags \
-    --resource-arns "${ARN}" \
-    --tags Key=env,Value=feature Key=service,Value=backend-api Key=version,Value=${DD_VERSION}
+DD_VERSION="pr-${PR_NUM}"
+aws elbv2 add-tags \
+  --resource-arns "${ARN}" \
+  --tags Key=env,Value=feature Key=service,Value=backend-api Key=version,Value="${DD_VERSION}"
 
 
-  # GDPR and HIPAA security group rules
-  ALB_SG_ID=$(aws elbv2 describe-load-balancers --load-balancer-arns "${ARN}" --query "LoadBalancers[*].[SecurityGroups]" --output text)
+# GDPR and HIPAA security group rules
+ALB_SG_ID=$(aws elbv2 describe-load-balancers --load-balancer-arns "${ARN}" --query "LoadBalancers[*].[SecurityGroups]" --output text)
 
-  CLUSTERS=$(aws ecs list-clusters --query "clusterArns" --output text)
-  for CLUSTER in ${CLUSTERS}; do
-    TAGS=$(aws ecs list-tags-for-resource --resource-arn ${CLUSTER} --query "tags[?key=='copilot-environment' && value=='pr-${PR_NUM}']" --output text)
-    if [ -n "${TAGS}" ]; then
-      echo "Cluster ARN: ${CLUSTER}"
-      SERVICE_ARN=$(aws ecs list-services --cluster "${CLUSTER}" --query "serviceArns[0]" --output text)
-      SGS=$(aws ecs describe-services --cluster "${CLUSTER}" --service "${SERVICE_ARN}"  --query "services[0].networkConfiguration.awsvpcConfiguration.securityGroups" --output text)
-      for SG in ${SGS}; do
-        if [[ "${SG}" = "sg-0fe1e219a0c0c5afb" ]]; then continue; fi
-        echo "Processing Security group: ${SG}, limiting access from ${ALB_SG_ID}"
-        echo "Removing existing inbound rules"
-        IP_PERMS=$(aws ec2 describe-security-groups --group-ids "${SG}" --query "SecurityGroups[0].IpPermissions" --output json)
+CLUSTERS=$(aws ecs list-clusters --query "clusterArns" --output text)
+for CLUSTER in ${CLUSTERS}; do
+  TAGS=$(aws ecs list-tags-for-resource --resource-arn ${CLUSTER} --query "tags[?key=='copilot-environment' && value=='pr-${PR_NUM}']" --output text)
 
-        aws ec2 revoke-security-group-ingress --group-id "${SG}" --ip-permissions "${IP_PERMS}" > /dev/null
+  if [ -n "${TAGS}" ]; then
+    echo "Cluster ARN: ${CLUSTER}"
+    SERVICE_ARN=$(aws ecs list-services --cluster "${CLUSTER}" --query "serviceArns[0]" --output text)
+    SGS=$(aws ecs describe-services --cluster "${CLUSTER}" --service "${SERVICE_ARN}"  --query "services[0].networkConfiguration.awsvpcConfiguration.securityGroups" --output text)
 
-        echo "Creating new ingress rule"
-        aws ec2 authorize-security-group-ingress \
-          --group-id "${SG}" \
-          --protocol tcp \
-          --port 1024-65535 \
-          --source-group "${ALB_SG_ID}" > /dev/null
-      done
-    fi
-  done
+    for SG in ${SGS}; do
+      if [[ "${SG}" = "sg-0fe1e219a0c0c5afb" ]]; then continue; fi
+      echo "Processing Security group: ${SG}, limiting access from ${ALB_SG_ID}"
+      echo "Removing existing inbound rules"
+      IP_PERMS=$(aws ec2 describe-security-groups --group-ids "${SG}" --query "SecurityGroups[0].IpPermissions" --output json)
 
+      aws ec2 revoke-security-group-ingress --group-id "${SG}" --ip-permissions "${IP_PERMS}" > /dev/null
+
+      echo "Creating new ingress rule"
+      aws ec2 authorize-security-group-ingress \
+        --group-id "${SG}" \
+        --protocol tcp \
+        --port 1024-65535 \
+        --source-group "${ALB_SG_ID}" > /dev/null
+    done
+  fi
 done
