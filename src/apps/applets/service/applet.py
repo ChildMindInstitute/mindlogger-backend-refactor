ChildMindInstitute/mindlogger-backend-refactor

commit d3a676ab0f1606853dba3aa9a6a884b910342539

chore: Revise applet-event version linking logic (M2-8973) (M2-8983) (M2-9047) (#1820)
This PR revises the logic that links versions of schedule events and applets together in the `applet_events` table. The current logic has a bug where an update to an applet that deletes an activity/flow results in the events for that activity/flow being linked to the new version of the applet in `applet_events`.

The root cause is that the linking action was being done before the events for the activity/flow were marked as deleted. This PR moves the linking action to the end of the update method to account for this.

I also took the opportunity to fix the `subject_id` field filter in the schedule history endpoints. The `subject_id` filter was missing the None condition because similar to `user_id`, when filtering by `subject_id` the event object should be returned when the field matches, or when it is null

src/apps/applets/service/applet.py
@@ -196,9 +196,6 @@ class AppletService:
         applet = await self._update(applet_id, update_data, next_version)
         await AppletHistoryService(self.session, applet.id, applet.version).add_history(self.user_id, applet)
 
-        if next_version != old_applet_schema.version:
-            await ScheduleHistoryService(self.session).update_applet_event_links(applet_id, applet.version)
-
         applet.activities = await ActivityService(self.session, self.user_id).update_create(
             applet_id, update_data.activities
         )
@@ -228,6 +225,12 @@ class AppletService:
             )
         )
         await asyncio.gather(*to_await)
+
+        if next_version != old_applet_schema.version:
+            await ScheduleHistoryService(self.session).update_applet_event_links(
+                applet_id=applet_id, current_applet_version=old_applet_schema.version, new_applet_version=applet.version
+            )
+
         return applet
 
     async def update_encryption(self, applet_id: uuid.UUID, encryption: Encryption):

src/apps/schedule/crud/schedule_history.py
@@ -17,7 +17,7 @@ from apps.schedule.db.schemas import (
     ReminderHistorySchema,
 )
 from apps.schedule.domain.schedule.public import ExportEventHistoryDto
-from apps.shared.filtering import Comparisons, FilterField, Filtering
+from apps.shared.filtering import FilterField, Filtering
 from apps.shared.paging import paging
 from apps.shared.query_params import QueryParams
 from apps.subjects.db.schemas import SubjectSchema
@@ -25,11 +25,11 @@ from infrastructure.database import BaseCRUD
 
 
 class _ScheduleHistoryExportFilters(Filtering):
-    respondent_ids = FilterField(EventHistorySchema.user_id, method_name="filter_respondent_ids")
-    subject_ids = FilterField(SubjectSchema.id, Comparisons.IN)
+    respondent_ids = FilterField(EventHistorySchema.user_id, method_name="filter_nullable_ids")
+    subject_ids = FilterField(SubjectSchema.id, method_name="filter_nullable_ids")
 
-    def filter_respondent_ids(self, field, value):
-        return or_(field.in_(value), EventHistorySchema.user_id.is_(None))
+    def filter_nullable_ids(self, field, value):
+        return or_(field.in_(value), field.is_(None))
 
 
 class ScheduleHistoryCRUD(BaseCRUD[EventHistorySchema]):
@@ -148,6 +148,17 @@ class ScheduleHistoryCRUD(BaseCRUD[EventHistorySchema]):
 
 
 class AppletEventsCRUD(BaseCRUD[AppletEventsSchema]):
+    async def find_by_applet_id_version(self, applet_id_version: str) -> list[AppletEventsSchema]:
+        query = select(AppletEventsSchema)
+        query = query.where(
+            AppletEventsSchema.applet_id == applet_id_version,
+            AppletEventsSchema.soft_exists(),
+        )
+
+        res = await self._execute(query)
+
+        return res.scalars().all()
+
     async def add(self, applet_event: AppletEventsSchema) -> AppletEventsSchema:
         return await self._create(applet_event)
 

src/apps/schedule/service/schedule_history.py
@@ -3,7 +3,6 @@ __all__ = ["ScheduleHistoryService"]
 import uuid
 
 from apps.applets.crud import AppletsCRUD
-from apps.schedule.crud.events import EventCRUD
 from apps.schedule.crud.schedule_history import (
     AppletEventsCRUD,
     NotificationHistoryCRUD,
@@ -100,22 +99,23 @@ class ScheduleHistoryService:
     async def update_applet_event_links(
         self,
         applet_id: uuid.UUID,
-        applet_version: str,
+        current_applet_version: str,
+        new_applet_version: str,
     ):
         """
         Make new entries into `applet_events` to link a new version of an applet to the existing version of its events.
         This method is useful when an applet has its version bumped and the events are not updated. The previous entries
         in `applet_events` are not removed to maintain the history of the applet.
         """
-        events = await EventCRUD(self.session).get_all_by_applet_id(applet_id)
 
-        if len(events) > 0:
-            await AppletEventsCRUD(self.session).add_many(
+        crud = AppletEventsCRUD(self.session)
+        applet_event_schemas = await crud.find_by_applet_id_version(f"{applet_id}_{current_applet_version}")
+
+        if len(applet_event_schemas) > 0:
+            await crud.add_many(
                 [
-                    AppletEventsSchema(
-                        applet_id=f"{applet_id}_{applet_version}", event_id=f"{event.id}_{event.version}"
-                    )
-                    for event in events
+                    AppletEventsSchema(applet_id=f"{applet_id}_{new_applet_version}", event_id=schema.event_id)
+                    for schema in applet_event_schemas
                 ]
             )
 

commit 57a51f0fd1cbb12c96637ff5b7fa014770e83a0e

fix: Link old event versions to new applet versions (M2-9135) #1833
This PR fixes a regression introduced by #1820 wherein old event version weren't being linked to the new applet versions in the `applet_events` table. The bug happens when the applet version is increased by creating a new activity/flow.

src/apps/applets/service/applet.py
@@ -187,6 +187,7 @@ class AppletService:
 
     async def update(self, applet_id: uuid.UUID, update_data: AppletUpdate) -> AppletFull:
         old_applet_schema = await AppletsCRUD(self.session).get_by_id(applet_id)
+        old_applet_version = old_applet_schema.version
 
         next_version = await self.get_next_version(old_applet_schema.version, update_data, applet_id)
 
@@ -226,9 +227,9 @@ class AppletService:
         )
         await asyncio.gather(*to_await)
 
-        if next_version != old_applet_schema.version:
+        if next_version != old_applet_version:
             await ScheduleHistoryService(self.session).update_applet_event_links(
-                applet_id=applet_id, current_applet_version=old_applet_schema.version, new_applet_version=applet.version
+                applet_id=applet_id, current_applet_version=old_applet_version, new_applet_version=applet.version
             )
 
         return applet