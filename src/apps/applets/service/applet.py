fff1ec244be0676e1e30adac2087a6794c85e529
sultanofcardio <14842108+sultanofcardio@users.noreply.github.com>
2025-04-30 03:49:25 -0500

    Store old applet version in separate variable

--- src/apps/applets/service/applet.py
+++ src/apps/applets/service/applet.py
@@ -187,6 +187,7 @@ class AppletService:
 
     async def update(self, applet_id: uuid.UUID, update_data: AppletUpdate) -> AppletFull:
         old_applet_schema = await AppletsCRUD(self.session).get_by_id(applet_id)
+        old_applet_version = old_applet_schema.version
 
         next_version = await self.get_next_version(old_applet_schema.version, update_data, applet_id)
 
@@ -226,9 +227,9 @@ class AppletService:
         )
         await asyncio.gather(*to_await)
 
-        if next_version != old_applet_schema.version:
+        if next_version != old_applet_version:
             await ScheduleHistoryService(self.session).update_applet_event_links(
-                applet_id=applet_id, current_applet_version=old_applet_schema.version, new_applet_version=applet.version
+                applet_id=applet_id, current_applet_version=old_applet_version, new_applet_version=applet.version
             )
 
         return applet
