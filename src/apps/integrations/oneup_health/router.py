c2eae76f7d30aaf25d6ca76d4bf286f2ab847290
André Vital <andre@andrevital.com>
2025-05-14 12:41:30 -0600

    feat: Add refresh_token endpoint

--- src/apps/integrations/oneup_health/router.py
+++ src/apps/integrations/oneup_health/router.py
@@ -1,7 +1,12 @@
 from fastapi.routing import APIRouter
 from starlette import status
 
-from apps.integrations.oneup_health.api import retrieve_token, retrieve_token_by_submit_id, trigger_data_fetch
+from apps.integrations.oneup_health.api import (
+    refresh_token,
+    retrieve_token,
+    retrieve_token_by_submit_id,
+    trigger_data_fetch,
+)
 from apps.integrations.oneup_health.domain import OneupHealthToken
 from apps.shared.domain import Response
 from apps.shared.domain.response import AUTHENTICATION_ERROR_RESPONSES, DEFAULT_OPENAPI_RESPONSE
@@ -30,6 +35,16 @@ router.get(
     },
 )(retrieve_token_by_submit_id)
 
+router.post(
+    "/refresh_token",
+    description="This endpoint is used to refresh an expired 1UpHealth access token using a refresh token",
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {"model": Response[OneupHealthToken]},
+        **DEFAULT_OPENAPI_RESPONSE,
+        **AUTHENTICATION_ERROR_RESPONSES,
+    },
+)(refresh_token)
 
 router.get(
     "/applet/{applet_id}/submissions/{submit_id}/trigger_data_fetch",
