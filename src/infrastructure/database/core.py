f12fe03a6f123581e03a1a73e9b1aba95831c619
Andrew Weiland <aweiland@users.noreply.github.com>
2025-07-08 14:04:59 -0400

    fix: Disabling connection pooling

--- src/infrastructure/database/core.py
+++ src/infrastructure/database/core.py
@@ -2,6 +2,7 @@ import json
 
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool.impl import NullPool
 
 from config import settings
 
@@ -14,9 +15,7 @@ def build_engine(uri: str) -> AsyncEngine:
         future=True,
         pool_pre_ping=True,
         echo=False,
-        pool_size=settings.database.pool_size,
-        max_overflow=settings.database.pool_overflow_size,
-        pool_timeout=settings.database.pool_timeout,
+        poolclass=NullPool,
         json_serializer=lambda x: json.dumps(x),
         json_deserializer=lambda x: json.loads(x),
     )
