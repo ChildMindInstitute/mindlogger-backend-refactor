d3e66947cf70c54fac80f2cb2f33f1a01e6c8dcc
Chris Lei <chris.lei@childmind.org>
2026-02-27 14:14:35 -0800

    uv run alembic -c alembic_arbitrary.ini revision

---
+++ src/infrastructure/database/migrations_arbitrary/versions/2026_02_27_13_59-add_missing_indexes_to_answers_table.py
@@ -0,0 +1,23 @@
+"""Add missing indexes to answers table
+
+Revision ID: 4e194e2a1dab
+Revises: b07ca71c94df
+Create Date: 2026-02-27 13:59:43.986247
+
+"""
+
+
+
+# revision identifiers, used by Alembic.
+revision = "4e194e2a1dab"
+down_revision = "b07ca71c94df"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
