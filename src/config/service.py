d5bd4e9c3f3982482a905044dc2d6dd0284054e8
Chris Lei <chris.lei@childmind.org>
2026-06-17 14:57:05 -0700

    {web.mindlogger.org => web.gettingcurious.com}

--- src/config/service.py
+++ src/config/service.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class FrontendUrlsSettings(BaseModel):
-    web_base: str = "web.mindlogger.org"
+    web_base: str = "web.gettingcurious.com"
     admin_base: str = "admin.frontend.com"
     invitation_send: str = "invitation"
     web_password_recovery_send: str = "password-recovery"
