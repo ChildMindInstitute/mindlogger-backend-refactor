35bdca4eafb89a4329f8f02e2d2a35de8245d261
Andrew Weiland <aweiland@users.noreply.github.com>
2026-09-21 16:19:34 -0400

    A test that tests a little

--- tests/integration/test_this.py
+++ tests/integration/test_this.py
@@ -1,2 +1,9 @@
-def test_something(app):
+import uuid
+
+
+def test_something(create_authorized_client):
+    client = create_authorized_client(uuid.uuid4())
+    response = client.get("/readiness")
+    if response.status_code != 200:
+        raise Exception("Readiness check failed")
     assert True
